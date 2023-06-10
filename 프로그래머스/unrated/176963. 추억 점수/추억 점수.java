344d2c923a2108120820822be645e13bf8d017ee
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-06-10 23:15:30 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,23 +1,21 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(String[] name, int[] yearning, String[][] photo) {
-        // 작품에 따라 점수를 2차배열로 설정해야함.
-        String[][] point = new String[name.length][2];
-        int sum = 0;
-        List<Integer> list = new ArrayList<>();
+        HashMap<String,Integer> map = new HashMap<>();
         for(int i=0; i<name.length; i++){
-            point[i][0] = name[i]; // 작품 이름과
-            point[i][1] = yearning[i]+""; // 그 작품의 점수를 넣음
+            map.put(name[i], yearning[i]);
         }
+        List<Integer> result = new ArrayList<>();
+        int sum = 0;
         for(int i=0; i<photo.length; i++){
             for(int j=0; j<photo[i].length; j++){
-                for(int k=0; k<name.length; k++){
-                    if(name[k].equals(photo[i][j])) sum+=Integer.parseInt(point[k][1]);
+                if(map.containsKey(photo[i][j])){
+                    sum+=map.get(photo[i][j]);
                 }
             }
-            list.add(sum);
-            sum=0;
+            result.add(sum);
+            sum = 0;
         }
-        return list;
+        return result;
     }
 }
