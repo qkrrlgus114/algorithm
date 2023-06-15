430b75db5ec0850773d0f01f34069f730b746fed
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-06-15 12:54:48 +0900

    [unrated] Title: 덧칠하기, Time: 1.18 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int count = 1;
+        int temp = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i=temp+1; i<section.length; i++){
+            if(section[i]-section[temp] < m){ // 범위 내에 색칠 가능함.
+                if(i==section.length-1) break; 
+            }else if(section[i]-section[temp] >= m){ // 범위 내에 색칠이 불가능함.
+                count++;
+                temp = i;
+            }
+        }
+        return count;
+    }
+}
