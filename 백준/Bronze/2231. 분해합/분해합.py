3316357a2b5dfe587e56f8f2fb06cbdfa5eac29e
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-06-18 22:21:38 +0900

    [Bronze II] Title: 분해합, Time: 780 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,11 +1,15 @@
 N = int(input())
 result = 0
-for i in range(1,N+1):
-  N_sum = sum(map(int, str(i)))
-  if N_sum+i == N:
-    result = i
+for i in range(N//3,N+1):
+  temp = i
+  sum = 0
+  while i>=1:
+    sum+=i%10
+    i=i//10
+  if sum+temp == N:
+    result = temp
     break
-if result != 0:
+if result!=0:
   print(result)
 else:
   print(0)
