cf938c11d8c46c4c408ed33df5969687a87b2e39
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-06-19 14:10:35 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,17 +1,8 @@
-def change(i, j, arr):
-  temp = arr[i]
-  arr[i] = arr[j]
-  arr[j] = temp
-
 N = int(input())
 arr = []
 for i in range(N):
   arr.append(int(input()))
   
-for i in range(N-1):
-  for j in range(i+1,N):
-    if arr[i] > arr[j]:
-      change(i,j,arr)
-
+arr.sort()
 for i in range(N):
   print(arr[i])
