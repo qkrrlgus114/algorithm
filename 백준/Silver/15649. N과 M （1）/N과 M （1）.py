022101a1632492fa902fca8292801d1c59885441
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-07-03 16:42:21 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 36716 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,22 +1,10 @@
+from itertools import permutations
 import sys
-
-def back():
-    global result
-    # 종료 조건
-    if len(result) == M:
-        for i in range(len(result)):
-            print(result[i], end=" ")
-        print()
-        return    
-    # 재귀 조건
-    for i in range(1, N+1):
-        if i not in result:
-            result.append(i)
-            back()
-            result.pop()
 
 N, M = map(int, sys.stdin.readline().strip().split())
 
-result = []
-
-back()
+result = list(permutations(range(1, N+1),M))
+for i in result:
+    for j in i:
+        print(j, end=" ")
+    print()
