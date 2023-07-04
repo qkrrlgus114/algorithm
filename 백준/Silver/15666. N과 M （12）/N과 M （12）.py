ce2e08c94e7919bc80dc244a84367f747155aa0a
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-07-05 00:19:52 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,26 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().strip().split())
+
+arr = list(map(int, sys.stdin.readline().strip().split()))
+arr.sort()
+visited = [False] * N
+
+result = []
+# 1 7 9 9
+def back(start):
+  # 종료
+  if len(result) == M:
+    for i in range(len(result)):
+      print(result[i],end=" ")
+    print()
+    return
+  
+  # 재귀
+  for i in range(start, N):
+    if i > 0 and arr[i-1] == arr[i]: continue
+    result.append(arr[i])
+    back(i)
+    result.pop()
+
+back(0)
