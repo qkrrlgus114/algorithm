b8b999a32f296f0163e2a2759a6447af0682e323
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-07-04 19:47:37 +0900

    [Silver III] Title: N과 M (7), Time: 3108 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,22 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().strip().split())
+temp = list(map(int, sys.stdin.readline().strip().split()))
+temp.sort()
+result = []
+
+def back():
+  # 종료
+  if len(result) == M:
+    for i in range(len(result)):
+      print(result[i], end=" ")
+    print()
+    return
+  
+  # 재귀
+  for i in range(0, N):
+    result.append(temp[i])
+    back()
+    result.pop()
+    
+back()
