a444833fe7daae1d1f4b47de0c3ff6ab326168a3
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-07-05 00:12:02 +0900

    [Silver II] Title: N과 M (9), Time: 160 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,28 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().strip().split())
+
+arr = list(map(int, sys.stdin.readline().strip().split()))
+arr.sort()
+visited = [False] * N
+
+result = []
+# 1 7 9 9
+def back():
+  # 종료
+  if len(result) == M:
+    for i in range(len(result)):
+      print(result[i],end=" ")
+    print()
+  
+  # 재귀
+  for i in range(N):
+    if i > 0 and arr[i] == arr[i-1] and visited[i-1] == False: continue
+    if visited[i] == False:
+      visited[i] = True
+      result.append(arr[i])
+      back()
+      result.pop()
+      visited[i] = False
+
+back()
