7b531445532102073b8ad0ead8618af78bf967f6
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-08-26 00:00:03 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,17 +1,22 @@
+# 동전의 개수, 총합을 입력 받음.
+# 동전을 배열에 넣은 뒤 가장 큰 동전부터 차례대로 계산하면 최적의 값을 찾을 수 있음.
 import sys
-from collections import deque
 
-N, K = map(int,sys.stdin.readline().strip().split())
-
-coin = deque()
-
-for i in range(N):
+N, K = map(int, sys.stdin.readline().split())
+coin = []
+for _ in range(N):
   coin.append(int(sys.stdin.readline()))
-  
-sum = 0
-coin.reverse()
+
+# 가장 큰 값 부터 뽑아내기 위해서  
+coin.sort(reverse=True)
+
+# 총 동전의 개수
+count = 0
 
 for i in range(N):
-  sum = sum + K//coin[i]
-  K = K%coin[i]
-print(sum)
+  if K >= coin[i]:
+    temp = K // coin[i]
+    count += temp
+    K %= coin[i]
+    
+print(count)
