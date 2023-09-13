65b4c6d5dc8999267679eabcbb3e388a26ddb8c2
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-09-13 21:28:04 +0900

    [Silver V] Title: 수들의 합, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,14 +1,13 @@
 import sys
 
 N = int(sys.stdin.readline())
+
+temp = 1
 sum = 0
-if N == 1 or N == 2:
-  print(1)
-elif N == 3:
-  print(2)
-else:
-  for i in range(1,N):
-    sum+=i
-    if sum>N:
-      print(i-1)
-      break
+while True:
+    
+    sum += temp
+    if sum > N:
+        print(temp - 1)
+        break
+    temp += 1
