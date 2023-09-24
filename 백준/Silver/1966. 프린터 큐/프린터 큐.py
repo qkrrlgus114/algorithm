8cb34de8ec879d12715834c6720109e923a1aa4a
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-09-24 15:25:49 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,27 +1,22 @@
 import sys
 from collections import deque
 
-T = int(sys.stdin.readline().strip())
-
+T = int(sys.stdin.readline())
 for _ in range(T):
-  N, M = map(int,sys.stdin.readline().strip().split())
+    N, target_idx = map(int, sys.stdin.readline().split())
 
-  q = deque(list(map(int,sys.stdin.readline().strip().split())))
-  count = 1
-  while len(q)>0:
-    max_value = max(q)
-    temp = q.popleft()
-    if M==0:
-      if temp >= max_value:
-        print(count)
-        break
-      elif temp < max_value:
-        q.append(temp)
-        M = len(q)-1
-    else:
-      if temp >= max_value: # 뽑아낸 값이 크면
-        M-=1
-        count += 1
-      elif temp < max_value: # 뽑아낸 값이 작으면 뒤로
-        q.append(temp)
-        M-=1
+    paper = deque(list(map(int, sys.stdin.readline().split())))
+    paper_idx = deque(list(range(N)))
+    cnt = 0
+
+    while paper:
+        if paper[0] == max(paper):
+            cnt += 1
+            paper.popleft()
+            temp = paper_idx.popleft()
+            if temp == target_idx:
+                print(cnt)
+                break
+        else:
+            paper.append(paper.popleft())
+            paper_idx.append(paper_idx.popleft())
