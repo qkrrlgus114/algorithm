6b2977d2c81609f776155a9e0305ded5fc097230
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-10-09 00:09:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,3 +1,14 @@
-temp = input()
-for i in "abcdefghijklmnopqrstuvwxyz":
-  print(temp.find(i), end=" ")
+import sys
+
+word = list(sys.stdin.readline().strip())
+
+temp = 0
+
+count = [-1] * 27
+for i in word:
+    if count[ord(i) - 97] == -1:
+        count[ord(i) - 97] = temp
+    temp += 1
+
+for i in range(26):
+    print(count[i], end=" ")
