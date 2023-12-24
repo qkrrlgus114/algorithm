106cd4285897a2c046e582351cd37b573ffce03d
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2023-12-24 15:53:41 +0900

    [level 1] Title: 문자열 나누기, Time: 0.49 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,48 +3,27 @@ import java.io.*;
 
 class Solution {
     public int solution(String s) {
-        int result = 0;
-        int equalsCount = 0;
-        int notEqualsCount = 0;
         char first = ' ';
-        // 현재 잘랐는지 안 잘랐는지 판단
-        boolean check = false;
+        int same = 0;
+        int different = 0;
+        int result = 0;
         
-        while(true){
-            for(int i=0; i<s.length(); i++){
-                if(i == 0){
-                    first = s.charAt(i);
-                    equalsCount++;
-                    continue;
-                }
-                
-                if(first == s.charAt(i)){
-                    equalsCount++;
-                }else{
-                    notEqualsCount++;
-                }
-                
-                if(equalsCount == notEqualsCount){
-                    s = s.substring(i+1, s.length());
-                    result++;
-                    check = true;
-                    equalsCount = 0;
-                    notEqualsCount = 0;
-                    first = ' ';
-                    break;
-                }
+        for(char c : s.toCharArray()){
+            if(first == ' '){
+                first = c;
+                result++;
+                same++;
+            }else if(first == c){
+                same++;
+            }else{
+                different++;
             }
             
-            if(!check){
-                if(s.length() == 0){
-                    break;
-                }
-                result++;
-                break;
-            }else{
-                check = false;
+            if(same == different){
+                first = ' ';
             }
         }
+        
         return result;
         
     }
