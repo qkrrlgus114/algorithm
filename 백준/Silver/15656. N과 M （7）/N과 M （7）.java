dd9979353507f98261d7575d51a714a88cb4f7c8
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-01-04 16:13:05 +0900

    [Silver III] Title: N과 M (7), Time: 924 ms, Memory: 290420 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    static int[] arr;
+    static int[] result;
+    static boolean[] visited;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bf.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        arr = new int[N];
+        result = new int[M];
+        visited = new boolean[N];
+        s = bf.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(arr);
+        back(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void back(int depth, int count) {
+        if (count == M) {
+            for (Integer i : result) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+//            if(!visited[i]){
+//                visited[i] = true;
+                result[count] = arr[i];
+                back(i, count + 1);
+//                visited[i] = false;
+            }
+        }
+    }
