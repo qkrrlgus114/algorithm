34c4604be9d33b77a0ba5a7817bb0dc625cf1568
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-01-04 16:31:37 +0900

    [Silver II] Title: N과 M (10), Time: 124 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+public class Main {
+    static int[] arr;
+    static int[] result;
+    static boolean[] visited;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bf.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        arr = new int[N];
+        result = new int[M];
+        visited = new boolean[N];
+        
+        s = bf.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(arr);
+        back(0, 0);
+        System.out.print(sb.toString());
+    }
+
+    public static void back(int depth, int count) {
+        if (count == M) {
+            for (Integer i : result) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        // 과거의 값
+        int past = -1;
+
+        for (int i = depth; i < N; i++) {
+            if (!visited[i] && past != arr[i]) {
+                past = arr[i];
+                visited[i] = true;
+                result[count] = arr[i];
+                back(i, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
