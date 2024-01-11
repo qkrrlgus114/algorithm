1405d2a04244803f656f87bb415d4aed709818b5
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-01-11 16:18:51 +0900

    [Silver IV] Title: 스택 2, Time: 1268 ms, Memory: 314148 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -8,11 +8,13 @@ import java.util.Stack;
 
 public class Main {
 
-    static Stack<Integer> s = new Stack<>();
+    static List<Integer> s = new ArrayList<>();
+    static int top = -1;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         // 명령의 개수
         String n = bf.readLine();
@@ -24,27 +26,15 @@ public class Main {
 
             if(index == 1){
                 int value = Integer.parseInt(sa[1]);
-                s.push(value);
+                push(value);
             }else if(index == 2){
-                if(!s.isEmpty()){
-                    sb.append(s.pop() + "\n");
-                }else{
-                    sb.append(-1 + "\n");
-                }
+                pop();
             }else if(index == 3){
-                sb.append(s.size() + "\n");
+                size();
             }else if(index == 4){
-                if(s.isEmpty()){
-                    sb.append(1 + "\n");
-                }else{
-                    sb.append(0 + "\n");
-                }
+                sb.append(isEmpty() + "\n");
             }else{
-                if(!s.isEmpty()){
-                    sb.append(s.peek() + "\n");
-                }else{
-                    sb.append(-1 + "\n");
-                }
+                peek();
             }
         }
 
@@ -53,44 +43,44 @@ public class Main {
 
     }
 
-//    // 추가
-//    public static void push(int value){
-//        s.add(value);
-//        top++;
-//    }
-//
-//    // 비었는지 체크
-//    public static int isEmpty(){
-//        if(top == -1){
-//            return 1;
-//        }else{
-//            return 0;
-//        }
-//    }
-//
-//    // 스택 pop 구현
-//    public static void pop(){
-//        if(isEmpty() == 0){
-//            System.out.println(s.get(top));
-//            s.remove(top--);
-//        }else{
-//            System.out.println(-1);
-//        }
-//    }
-//
-//    // 스택 사이즈 출력
-//    public static void size(){
-//        System.out.println(top + 1);
-//    }
-//
-//    // 스택 peek 구현
-//    public static void peek(){
-//        if(isEmpty() == 0){
-//            System.out.println(s.get(top));
-//        }else{
-//            System.out.println(-1);
-//        }
-//    }
+    // 추가
+    public static void push(int value){
+        s.add(value);
+        top++;
+    }
+
+    // 비었는지 체크
+    public static int isEmpty(){
+        if(top == -1){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    // 스택 pop 구현
+    public static void pop(){
+        if(isEmpty() == 0){
+            sb.append(s.get(top) + "\n");
+            s.remove(top--);
+        }else{
+            sb.append(-1 + "\n");
+        }
+    }
+
+    // 스택 사이즈 출력
+    public static void size(){
+        sb.append((top + 1) + "\n");
+    }
+
+    // 스택 peek 구현
+    public static void peek(){
+        if(isEmpty() == 0){
+            sb.append(s.get(top) + "\n");
+        }else{
+            sb.append(-1 + "\n");
+        }
+    }
 
 
 }
