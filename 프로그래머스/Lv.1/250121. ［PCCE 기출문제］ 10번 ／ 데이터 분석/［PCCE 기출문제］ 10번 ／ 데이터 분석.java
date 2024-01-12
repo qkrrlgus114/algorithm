beaaff7fa664813869741bd76de5cf696b01cdf0
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-01-13 07:11:31 +0900

    [level Lv.1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.71 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/Lv.1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/Lv.1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -4,10 +4,10 @@ class Solution {
     static int[][] new_data;
     static int[] new_sort_arr;
     static int ext_count = 0;
+    static int sort_by_index = 0;
     
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        // code = 0, date = 1, maximum = 2, reamin = 3q
-        new_data = new int[data.length][4];
+        // code = 0, date = 1, maximum = 2, reamin = 3
         
         if(ext.equals("code")){
             check(data, 0, val_ext);    
@@ -19,33 +19,20 @@ class Solution {
             check(data, 3, val_ext);    
         }
         
-        int sort_by_index = 0;
+        
         
         if(sort_by.equals("code")) sort_by_index = 0;
         else if(sort_by.equals("date")) sort_by_index = 1;
         else if(sort_by.equals("maximum")) sort_by_index = 2;
         else sort_by_index = 3;
         
-        new_sort_arr = new int[ext_count];
-        
-        for(int i=0; i<ext_count; i++){
-            new_sort_arr[i] = new_data[i][sort_by_index];
-        }
-        
-        Arrays.sort(new_sort_arr);
-        
-        int[][] result_arr = new int[ext_count][4];
-        
-        for(int i=0; i<ext_count; i++){
-            for(int j=0; j<ext_count; j++){
-                if(new_sort_arr[i] == new_data[j][sort_by_index]){
-                    result_arr[i] = new_data[j];
-                }
+        Arrays.sort(new_data, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return Integer.compare(a[sort_by_index], b[sort_by_index]);
             }
-        }
+        });
         
-        
-        return result_arr;
+        return new_data;
     }
     
     public static void check(int[][] data, int index, int val_ext){
@@ -53,6 +40,11 @@ class Solution {
         for(int i=0; i<data.length; i++){
             if(val_ext > data[i][index]){
                 ext_count++;
+            }
+        }
+        new_data = new int[ext_count][4];
+        for(int i=0; i<data.length; i++){
+            if(val_ext > data[i][index]){
                 new_data[temp++] = data[i];
             }
         }
