03df8f0a54256be85e491a35192fdb56e326cfbf
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-03-04 17:09:22 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 812 ms, Memory: 57788 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+
+        int N = Integer.parseInt(s);
+        HashSet<String> set = new HashSet<>();
+
+        for(int i=0; i<N; i++){
+            String[] sa = bf.readLine().split(" ");
+            if("enter".equals(sa[1])) set.add(sa[0]);
+            else set.remove(sa[0]);
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(String name : list){
+            sb.append(name).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
