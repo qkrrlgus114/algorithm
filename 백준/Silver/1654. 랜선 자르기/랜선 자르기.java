a901f7cb237e798a84d9975c09a5c1b844912d59
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-03-08 21:10:47 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 17400 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,48 +1,51 @@
 import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    static int N;
-    static int K;
-    static long[] line;
-    static long max = Integer.MIN_VALUE;
+    static int N, K;
+    static long[] arr;
+    static long max = Long.MIN_VALUE;
+
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         N = Integer.parseInt(sa[0]);
         K = Integer.parseInt(sa[1]);
-        line = new long[N];
-        for (int i = 0; i < N; i++) {
-            String n = bf.readLine();
-            line[i] = Long.parseLong(n);
+        arr = new long[N];
+
+        int max_value = 0;
+
+        for(int i=0; i<N; i++){
+            String s = bf.readLine();
+            arr[i] = Integer.parseInt(s);
+            if(max_value < arr[i]) max_value = (int) arr[i];
         }
+        binarySearch(1, max_value);
 
-        Arrays.sort(line);
-
-        binarySearch(1L, line[N - 1]);
         System.out.println(max);
     }
 
-    public static void binarySearch(long start, long end) {
-        if(start > end){
-            return;
-        }
-        long mid = (start + end) / 2;
+    public static void binarySearch(long low, long high){
+        if(low > high) return;
 
-        int sum = 0;
-        for (int i = 0; i < line.length; i++) {
-            sum += line[i] / mid;
+        long mid = (low + high) / 2;
+        long M = 0;
+
+        for(long i : arr){
+            M += i/mid;
         }
 
-        if (sum < K) {
-            binarySearch(start, mid - 1);
-        } else {
-            if(max < mid) max = mid;
-            binarySearch(mid + 1, end);
+        if(M >= K){
+            max = Math.max(max, mid);
+            binarySearch(mid + 1, high);
+        }else{
+            binarySearch(low, mid - 1);
         }
     }
 }
