08558a8e8a9c5d297900bba950bba8d60858f947
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-03-11 20:56:02 +0900

    [Gold V] Title: 두 용액, Time: 396 ms, Memory: 29148 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -3,60 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    static Integer[] arr;
-    static int min = Integer.MAX_VALUE;
     static int result_one = 0;
     static int result_two = 0;
+    static int value = Integer.MAX_VALUE;
+    static int[] arr;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String s = bf.readLine();
         int N = Integer.parseInt(s);
-        arr = new Integer[N];
 
+        arr = new int[N];
         String[] sa = bf.readLine().split(" ");
+
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(sa[i]);
         }
 
         Arrays.sort(arr);
 
-        for(int i=0; i<N; i++){
-            binarySearch(i+1, N-1, arr[i], i);
+        for(int i=0; i<N-1; i++){
+            binarySearch(i+1, N-1, arr[i]);
         }
 
-        int a = Math.max(result_one, result_two);
-        int b = Math.min(result_one, result_two);
+        System.out.println(Math.min(result_one, result_two) + " " + Math.max(result_one, result_two));
 
-        System.out.println(b + " " + a);
 
     }
 
-    public static void binarySearch(int low, int high, int target, int index){
+    public static void binarySearch(int low, int high, int target){
         if(low > high) return;
 
         int mid = (low + high) / 2;
-        if(index == mid) return;
 
-        int value = Math.abs(target + arr[mid]);
-        if(min > value){
-            min = value;
+        int temp = Math.abs(arr[mid] + target);
+
+        if(temp < value){
+            value = temp;
             result_one = target;
             result_two = arr[mid];
         }
 
         if(target + arr[mid] < 0){
-            binarySearch(mid + 1, high, target, index);
+            binarySearch(mid + 1, high, target);
+        }else if(target + arr[mid] > 0){
+            binarySearch(low, mid - 1, target);
         }
 
-        if(target + arr[mid] > 0) {
-            binarySearch(low, mid - 1, target, index);
-        }
     }
 }
