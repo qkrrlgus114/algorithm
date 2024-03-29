aaeae859b7da6ab14b84fd483fcc9f510693e9b2
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-03-29 09:21:37 +0900

    [Gold V] Title: 전구와 스위치, Time: 672 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -17,77 +18,79 @@ public class Main {
         N = Integer.parseInt(s);
 
         status = bf.readLine();
-        List<Integer> cur_list = new ArrayList<>();
-        cur_list = switchInput(cur_list);
 
-        List<Integer> push_list = new ArrayList<>();
-        push_list = switchInput(push_list);
-        List<Integer> no_push_list = new ArrayList<>();
-        no_push_list = switchInput(no_push_list);
-
-        status = bf.readLine();
-        List<Integer> target_list = new ArrayList<>();
-        target_list = switchInput(target_list);
-
+        // 현재 상태
+        boolean[] cur_arr = new boolean[N];
+        cur_arr = input(cur_arr);
+        // 초반에 누른 스위치
+        boolean[] temp1 = new boolean[N];
+        temp1 = input(temp1);
+        temp1 = switchOn(temp1, 0);
         push_count = 1;
+        // 초반에 안 누른 스위치
+        boolean[] temp2 = new boolean[N];
+        temp2 = input(temp2);
         no_push_count = 0;
 
-        switchOn(push_list, 0);
+        // 목표하는 상태
+        status = bf.readLine();
+        boolean[] target_arr = new boolean[N];
+        target_arr = input(target_arr);
 
+        boolean check1 = false;
+        boolean check2 = false;
         for(int i=1; i<N; i++){
-            if(check(push_list, target_list)) break;
-            if(check(no_push_list, target_list)) break;
-
-            if(push_list.get(i-1) != target_list.get(i-1)){
-                switchOn(push_list, i);
+            // 둘 중 하나라도 목표와 같으면 종료
+            if(Arrays.equals(temp1, target_arr)) {
+                check1 = true;
+                break;
+            }
+            if(Arrays.equals(temp2, target_arr)){
+                check2 = true;
+                break;
+            }
+            // temp1이 다르면 누름
+            if(temp1[i-1] != target_arr[i-1]){
+                temp1 = switchOn(temp1, i);
                 push_count++;
             }
-            if(no_push_list.get(i-1) != target_list.get(i-1)){
-                switchOn(no_push_list, i);
+            // temp2이 다르면 누름
+            if(temp2[i-1] != target_arr[i-1]){
+                temp2 = switchOn(temp2, i);
                 no_push_count++;
             }
         }
-
-        if(!check(push_list, target_list) && !check(no_push_list, target_list)) System.out.println("-1");
-        else if(check(push_list, target_list) && check(no_push_list, target_list)) System.out.println(Math.min(push_count, no_push_count));
-        else if(check(push_list, target_list)) System.out.println(push_count);
-        else if(check(no_push_list, target_list)) System.out.println(no_push_count);
+        if(Arrays.equals(temp1, target_arr) || check1) System.out.println(push_count);
+        else if(Arrays.equals(temp2, target_arr) || check2) System.out.println(no_push_count);
+        else System.out.println("-1");
     }
 
-    // 스위치 변경 함수
-    public static void switchOn(List<Integer> list, int index){
-        if(index > 0 && index < N - 1){
-            for(int i=index-1; i<=index+1; i++){
-                if(list.get(i) == 1) list.set(i, 0);
-                else list.set(i, 1);
+    // 입력
+    public static boolean[] input(boolean[] arr) {
+        for (int i = 0; i < N; i++) {
+            arr[i] = status.charAt(i) == '0' ? false : true;
+        }
+        return arr;
+    }
+
+    // 스위치 누름
+    public static boolean[] switchOn(boolean[] arr, int index) {
+        if (index == 0) {
+            for (int i = 0; i <= 1; i++) {
+                arr[i] = !arr[i];
             }
-        }else if(index == 0){
-            for(int i=0; i<=1; i++){
-                if(list.get(i) == 1) list.set(i, 0);
-                else list.set(i, 1);
+        } else if (index == N - 1) {
+            for (int i = N - 2; i <= N - 1; i++) {
+                arr[i] = !arr[i];
             }
-        }else if(index == N-1){
-            for(int i=N-2; i<=N-1; i++){
-                if(list.get(i) == 1) list.set(i, 0);
-                else list.set(i, 1);
+        } else {
+            for (int i = index - 1; i <= index + 1; i++) {
+                arr[i] = !arr[i];
             }
         }
+        return arr;
     }
 
-    // 스위치 입력
-    public static List<Integer> switchInput(List<Integer> list){
-        for(int i=0; i<status.length(); i++){
-            list.add(status.charAt(i) - '0');
-        }
-        return list;
-    }
-
-    public static boolean check(List<Integer> input, List<Integer> target){
-        for(int i=0; i<N; i++){
-            if(input.get(i) != target.get(i)) return false;
-        }
-        return true;
-    }
 }
 
 
