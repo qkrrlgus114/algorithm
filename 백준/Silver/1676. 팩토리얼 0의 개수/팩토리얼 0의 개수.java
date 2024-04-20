44f618ab08229ce622daea47c16c49ab728707a7
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-20 17:03:08 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+        int N = Integer.parseInt(s);
+        int M = N / 25;
+        int K = N / 125;
+
+        System.out.println(N / 5 + M + K);
+    }
+}
