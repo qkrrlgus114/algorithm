ec34c2a6214e241b0e276b810db58aad12d7292a
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-20 23:01:46 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,10 @@
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_INS AS A
-JOIN ANIMAL_OUTS AS B
-ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE A.DATETIME > B.DATETIME
-ORDER BY A.DATETIME
+# 보호 시작일 > 입양일 
+# id, 이름 조회
+# 보호시작일이 빠른 순
+
+select ai.animal_id, ai.name
+from animal_ins as ai
+inner join animal_outs as ao
+on ai.animal_id = ao.animal_id
+where ai.datetime > ao.datetime
+order by ai.datetime
