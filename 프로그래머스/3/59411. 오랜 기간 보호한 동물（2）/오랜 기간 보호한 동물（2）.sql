9264b2719f0a21f55541f4303c718d63ef4179ff
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-20 23:53:01 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,10 @@
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_INS AS A
-JOIN ANIMAL_OUTS AS B
-ON A.ANIMAL_ID = B.ANIMAL_ID
-ORDER BY DATEDIFF(B.DATETIME, A.DATETIME) DESC LIMIT 2
+# 입양을 간 동물 
+# 보호 기간이 가장 길었던 두 마리
+# 아이디, 이름 조회
+# 보호 기간이 긴 순으로
+
+select ai.animal_id, ai.name
+from animal_ins as ai
+inner join animal_outs as ao
+on ai.animal_id = ao.animal_id
+order by ao.datetime - ai.datetime desc limit 2
