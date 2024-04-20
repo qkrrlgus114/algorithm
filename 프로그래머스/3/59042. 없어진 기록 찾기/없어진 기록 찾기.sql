899b149676ad882310287673bd941e8dd6b0ee97
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-21 00:16:13 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,9 @@
--- 코드를 입력하세요
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_OUTS AS A
-LEFT JOIN ANIMAL_INS AS B
-ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE B.DATETIME IS NULL
+# 입양 간 기록은 있는데
+# 보호소에 들어온 기록이 없는 
+# 동물 id, 이름 id
+
+select ao.animal_id, ao.name
+from ANIMAL_OUTS as ao
+left join ANIMAL_INS as ai
+on ao.animal_id = ai.animal_id
+where ai.datetime is null
