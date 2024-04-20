c1649234d73cf0f11fcabbe5488eebf197092cf5
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-21 00:24:38 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,10 +1,14 @@
-SELECT B.WRITER_ID, U.NICKNAME, CONCAT(U.CITY," ", U.STREET_ADDRESS1, " ", U.STREET_ADDRESS2) AS '전체주소', CONCAT(SUBSTR(U.TLNO, 1, 3), "-", SUBSTR(U.TLNO, 4, 4) ,"-" , SUBSTR(U.TLNO, 8, 4)) AS '전화번호'
-FROM USED_GOODS_BOARD AS B
-JOIN USED_GOODS_USER AS U
-ON B.WRITER_ID = U.USER_ID
-WHERE B.WRITER_ID IN (SELECT WRITER_ID
-                     FROM USED_GOODS_BOARD
-                     GROUP BY WRITER_ID
-                     HAVING COUNT(*) >= 3)
-GROUP BY B.WRITER_ID, U.NICKNAME
-ORDER BY B.WRITER_ID DESC
+# 게시물 3건 이상 등록한
+# 사용자 id, 닉네임, 전체주소, 전화번호 조회
+# 회원 id 내림차순
+
+select distinct(u.user_id), u.nickname, concat(u.city," ", u.street_address1, " ", u.street_address2) as '전체주소', 
+concat(substring(u.tlno, 1, 3), "-", substring(u.tlno, 4, 4), "-", substring(u.tlno, 8, 4)) as '전화번호'
+from USED_GOODS_BOARD as b
+inner join USED_GOODS_USER as u
+on b.writer_id = u.user_id
+where b.writer_id in (select writer_id
+    from USED_GOODS_BOARD 
+    group by writer_id 
+    having count(writer_id) >= 3)
+order by u.user_id desc
