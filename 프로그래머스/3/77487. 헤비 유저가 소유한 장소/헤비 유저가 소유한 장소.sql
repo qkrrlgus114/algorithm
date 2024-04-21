c4a713882d7179cc99bea668527b8a2abb55a9a8
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-21 12:49:54 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,8 +1,11 @@
-SELECT *
-FROM PLACES
-WHERE HOST_ID = ANY(SELECT HOST_ID
-                   FROM PLACES
-                   GROUP BY HOST_ID
-                   HAVING COUNT(HOST_ID) >= 2)
-ORDER BY ID
+# 공간을 둘 이상 등록한 사람
+# 공간의 정보를 아이디 순으로
+
+select *
+from places
+where host_id in (select host_id
+    from places
+    group by host_id 
+    having count(host_id) >= 2)
+order by id asc
 
