1998e6a0626348df5ef64f7ce4d8b0f1479614cf
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-24 16:04:21 +0900

    [Silver V] Title: 배열 합치기, Time: 1484 ms, Memory: 312096 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,7 +1,11 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -11,47 +15,38 @@ public class Main {
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
 
-        int[] arr_n = new int[N];
-        int[] arr_m = new int[M];
-
+        int[] A = new int[N];
+        int[] B = new int[M];
 
         sa = bf.readLine().split(" ");
         for(int i=0; i<N; i++){
-            arr_n[i] = Integer.parseInt(sa[i]);
+            A[i] = Integer.parseInt(sa[i]);
         }
         sa = bf.readLine().split(" ");
         for(int i=0; i<M; i++){
-            arr_m[i] = Integer.parseInt(sa[i]);
+            B[i] = Integer.parseInt(sa[i]);
         }
 
         StringBuilder sb = new StringBuilder();
+        int left = 0;
+        int right = 0;
 
-        int l = 0;
-        int r = 0;
-         while(true){
-             if(l >= N && r >= M) break;
-
-             if(l >= N){
-                 sb.append(arr_m[r]).append(" ");
-                 r++;
-             }else if(r >= M){
-                 sb.append(arr_n[l]).append(" ");
-                 l++;
-             }else{
-                 int n = arr_n[l];
-                 int m = arr_m[r];
-                 if(n > m){
-                     sb.append(m).append(" ");
-                     r++;
-                 }else{
-                     sb.append(n).append(" ");
-                     l++;
-                 }
-             }
-         }
+        while(left < N || right < M){
+            if(left >= N){
+                sb.append(B[right++]).append(" ");
+            }else if(right >= M){
+                sb.append(A[left++]).append(" ");
+            }else{
+                if(A[left] >= B[right]){
+                    sb.append(B[right++]).append(" ");
+                }else{
+                    sb.append(A[left++]).append(" ");
+                }
+            }
+        }
 
         System.out.println(sb);
 
     }
-
 }
+
