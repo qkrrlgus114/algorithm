0ad21c6cead1bd5271223c8b263b355c725e2b64
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-04-30 22:01:43 +0900

    [Silver III] Title: 수열, Time: 280 ms, Memory: 25856 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,41 +1,40 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] sa = bf.readLine().split(" ");
-
         int N = Integer.parseInt(sa[0]);
         int K = Integer.parseInt(sa[1]);
-        int[] arr = new int[N];
 
+        int[] arr = new int[N+1];
         sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(sa[i]);
+        for(int i=1; i<N+1; i++){
+            arr[i] = Integer.parseInt(sa[i-1]);
         }
 
-        // 누적합 배열
-        int[] sum_arr = new int[N+1];
-        sum_arr[1] = arr[0];
-
-        for(int i=1; i<N; i++){
-            sum_arr[i+1] = sum_arr[i] + arr[i];
+        int[] pSum = new int[N+1];
+        for(int i=1; i<N+1; i++){
+            pSum[i] = pSum[i-1] + arr[i];
         }
 
-        // 최대값
-        int result = Integer.MIN_VALUE;
-        for(int i=0; i <= arr.length - K; i++){
-            if(result < sum_arr[i+K] - sum_arr[i]){
-                result = sum_arr[i+K] - sum_arr[i];
-            }
+        int answer = Integer.MIN_VALUE;
+        for(int i=K; i<N+1; i++){
+            int value = pSum[i] - pSum[i-K];
+            if(value > answer) answer = value;
         }
 
-        System.out.println(result);
-
+        System.out.println(answer);
     }
 
 }
+
