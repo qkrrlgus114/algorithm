5077b78b3d8b71bbe24c6918d5a28282fee32218
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-05-04 08:41:39 +0900

    [Gold V] Title: 빗물, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -17,33 +17,61 @@ public class Main {
         String[] sa = bf.readLine().split(" ");
         int H = Integer.parseInt(sa[0]);
         int W = Integer.parseInt(sa[1]);
-        int[][] ground = new int[H][W];
+        int[] ground = new int[W];
+
+        int start = 0;
+        int end = W - 1;
+        int maxHStart = 0;
+        int maxHEnd = 0;
+        int maxH = 0;
 
         sa = bf.readLine().split(" ");
-        for(int i=0; i<W; i++){
+        for(int i=0; i<sa.length; i++){
             int num = Integer.parseInt(sa[i]);
-            for(int j=H-1; j>H-1-num; j--){
-                ground[j][i] = 1;
+            ground[i] = num;
+
+            if(num > maxH){
+                maxHStart = i;
+                maxHEnd = i;
+                maxH = num;
+            }else if(num == maxH){
+                maxHEnd = i;
             }
         }
 
         int answer = 0;
 
-        for(int i=0; i<H; i++){
-            int count = 0;
-            boolean check = false;
-            for(int j=0; j<W; j++){
-                if(ground[i][j] == 1){
-                    if(!check) check = true;
-                    else {
-                        answer += count;
-                        count = 0;
-                    }
-                }else{
-                    if(check) count++;
-                }
+        // 처음 ~ 처음 맥스값
+        int nowH = 0;
+        for(int i=start; i<=maxHStart; i++){
+            if(ground[i] <= nowH){
+                answer += nowH - ground[i];
+            }else{
+                nowH = ground[i];
             }
         }
+
+        // 마지막 ~ 마지막 맥스값
+        nowH = 0;
+        for(int i=end; i>=maxHEnd; i--){
+            if(ground[i] <= nowH){
+                answer += nowH - ground[i];
+            }else{
+                nowH = ground[i];
+            }
+        }
+        
+        // 처음 맥스값 ~ 마지막 맥스값
+        nowH = 0;
+        for(int i=maxHStart; i<=maxHEnd; i++){
+            if(ground[i] <= nowH){
+                answer += nowH - ground[i];
+            }else{
+                nowH = ground[i];
+            }
+        }
+
+        
 
         System.out.println(answer);
 
