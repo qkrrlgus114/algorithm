e4cb81f74de8410b7eac37d67378e97e4bc83a14
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-05-06 13:26:51 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 952 ms, Memory: 143016 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,53 @@
-import com.sun.source.doctree.SummaryTree;
+import com.sun.security.jgss.GSSUtil;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
 
+        String[] sa = bf.readLine().split(" ");
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
-        int[][] arr = new int[N][N];
-        int[][] sum_arr = new int[N+1][N+1];
 
-        for(int i=0; i<N; i++){
+        int[][] arr = new int[N+1][N+1];
+        // 입력 받기
+        for(int i=1; i<=N; i++){
             sa = bf.readLine().split(" ");
-            for(int j=0; j<N; j++){
-                arr[i][j] = Integer.parseInt(sa[j]);
-                sum_arr[i+1][j+1] = arr[i][j];
+            for(int j=1; j<=N; j++){
+                arr[i][j] = Integer.parseInt(sa[j-1]);
             }
         }
 
-        // 누적합 배열 생셩
-
-        for(int i=1; i<N+1; i++){
-            for(int j=1; j<N+1; j++){
-                sum_arr[i][j] += sum_arr[i-1][j] - sum_arr[i-1][j-1] + sum_arr[i][j-1];
+        // 누적합 구하기
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                arr[i][j] = arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1] + arr[i][j];
             }
         }
-
-        for(int t=0; t<M; t++){
+        
+        StringBuilder sb = new StringBuilder();
+        for(int q=0; q<M; q++){
             sa = bf.readLine().split(" ");
             int x1 = Integer.parseInt(sa[0]);
             int y1 = Integer.parseInt(sa[1]);
             int x2 = Integer.parseInt(sa[2]);
             int y2 = Integer.parseInt(sa[3]);
-            int sum = sum_arr[x2][y2] - sum_arr[x2][y1-1] - sum_arr[x1-1][y2] + sum_arr[x1-1][y1-1];
-            System.out.println(sum);
-        }
-    }
 
+            int value = arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1];
+            sb.append(value).append("\n");
+        }
+
+        System.out.println(sb);
+
+        
+    }
 }
+
