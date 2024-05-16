fbe5b4e98242a652564bc622182baac90bf3dadf
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-05-16 20:39:04 +0900

    [Gold V] Title: 누가 이길까, Time: 712 ms, Memory: 46064 KB -BaekjoonHub

--- 백준/Gold/28449. 누가 이길까/누가 이길까.java
+++ 백준/Gold/28449. 누가 이길까/누가 이길까.java
@@ -46,15 +46,15 @@ public class Main {
 
     public static int lowerBound(int[] b, int target){
         int l = 0;
-        int r = b.length;
+        int r = b.length - 1;
 
-        while(l < r){
+        while(l <= r){
             int mid = (l + r) / 2;
 
             if(b[mid] < target){
                 l = mid + 1;
             }else{
-                r = mid;
+                r = mid - 1;
             }
         }
         return l;
@@ -62,14 +62,14 @@ public class Main {
 
     public static int upperBound(int[] b, int target){
         int l = 0;
-        int r = b.length;
+        int r = b.length - 1;
 
-        while(l < r){
+        while(l <= r){
             int mid = (l + r) / 2;
             if(b[mid] <= target){
                 l = mid + 1;
             }else{
-                r = mid;
+                r = mid - 1;
             }
         }
         return l;
