20ac3dc8a76f5a75138982b5006b4c2a22556c41
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-06-11 19:18:14 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 204 ms, Memory: 19612 KB -BaekjoonHub

---
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        char[] commands = bf.readLine().toCharArray();
+
+        Stack<Character> s = new Stack<>();
+        int LCount = 0;
+        int SCount = 0;
+        int result = 0;
+
+        for(int i=0; i<N; i++){
+            char command = commands[i];
+            if(command >= '1' && command <= '9'){
+                result++;
+            }else{
+                if(command == 'L'){
+                    s.add(command);
+                    LCount++;
+                }else if(command == 'S'){
+                    s.add(command);
+                    SCount++;
+                }else if(command == 'R'){
+                    if(LCount == 0) break;
+                    else{
+                        LCount -= 1;
+                        result++;
+                    }
+                }else if(command == 'K'){
+                    if(SCount == 0) break;
+                    else{
+                        SCount -= 1;
+                        result++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
+
+
