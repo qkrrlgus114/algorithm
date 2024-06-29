c485135eb72dcb5d22775035af5d9d5c1fc5e388
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-06-29 19:12:37 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 600 ms, Memory: 65476 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,62 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-class Cop{
-    private int start;
-    private int end;
-
-    public Cop(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public int getStart(){
-        return this.start;
-    }
-
-    public int getEnd(){
-        return this.end;
-    }
-}
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
+    static int N, M;
+    static int[] ground;
+    static int[] sums;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
-        int N = Integer.parseInt(sa[0]);
-        int M = Integer.parseInt(sa[1]);
 
-        int[] arr = new int[N+1];
-        int[] delta = new int[N+2];
+        String[] sa = bf.readLine().split(" ");
+        N = Integer.parseInt(sa[0]);
+        M = Integer.parseInt(sa[1]);
+        ground = new int[N + 1];
+        sums = new int[N + 2];
         sa = bf.readLine().split(" ");
-        for(int i=1; i<=N; i++){
-            arr[i] = Integer.parseInt(sa[i-1]);
+        for (int i = 1; i <= N; i++) {
+            ground[i] = Integer.parseInt(sa[i - 1]);
         }
 
-        int result = 0;
-
-        for(int i=0; i<M; i++) {
+        for (int i = 0; i < M; i++) {
             sa = bf.readLine().split(" ");
             int start = Integer.parseInt(sa[0]);
             int end = Integer.parseInt(sa[1]);
-            int k = Integer.parseInt(sa[2]);
-            delta[start] += k;
-            delta[end+1] += -k;
+            int H = Integer.parseInt(sa[2]);
+
+            sums[start] += H;
+            sums[end + 1] += -H;
         }
 
         int value = 0;
-        for(int i=1; i<=N; i++){
-            value += delta[i];
-            arr[i] += value;
+        for (int i = 1; i <= N; i++) {
+            value += sums[i];
+            ground[i] += value;
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i=1; i<=N; i++){
-            sb.append(arr[i]).append(" ");
+            sb.append(ground[i]).append(" ");
         }
+
         System.out.println(sb);
 
     }
