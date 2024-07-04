e2c1d580c729c878cda017e487d2b18a1f71fbfa
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-07-04 20:35:17 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 108 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -5,34 +5,39 @@ import java.util.*;
 
 public class Main {
 
+    static int[][] house;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(bf.readLine());
 
-        int[][] house = new int[15][15];
-        for(int i=0; i<15; i++){
-            Arrays.fill(house[i], 1);
-        }
-
-        for(int i=0; i<15; i++){
-            house[14][i] = i+1;
-        }
-        Arrays.deepToString(house);
-        for(int i=13; i>=0; i--){
-            for(int j=1; j<15; j++){
-                house[i][j] = house[i+1][j] + house[i][j-1];
-            }
-        }
+        house = new int[15][15];
 
         StringBuilder sb = new StringBuilder();
         for(int t=0; t<T; t++){
             int k = 14 - Integer.parseInt(bf.readLine());
             int n = Integer.parseInt(bf.readLine()) - 1;
 
-            sb.append(house[k][n]).append("\n");
+            int value = dp(k, n);
+
+            sb.append(value).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    private static int dp(int k, int n){
+        if(n == 0){
+            return house[k][n] = 1;
+        }
+        if(k == 14){
+            return house[k][n] = n + 1;
+        }
+        if(house[k][n] != 0){
+            return house[k][n];
+        }
+
+        return house[k][n] = dp(k+1, n) + dp(k, n-1);
     }
 
 }
