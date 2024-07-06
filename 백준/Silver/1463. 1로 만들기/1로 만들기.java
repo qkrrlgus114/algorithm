03d44ccb454b0b1720eba3092477c237ea4d88a1
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-07-06 17:53:12 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -10,25 +8,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
+        int N = Integer.parseInt(bf.readLine());
 
-        memo = new int[N+1];
+        memo = new int[N + 1];
+        if (N >= 3) {
+            memo[2] = memo[3] = 1;
+        } else if (N == 2) {
+            memo[2] = 1;
+        }
 
-        for(int i=2; i<=N; i++){
-            if(i % 6 == 0){
-                memo[i] = Math.min(Math.min(memo[i/3], memo[i/2]), memo[i-1]) + 1;
-            }
-            else if(i % 3 == 0){
-                memo[i] = Math.min(memo[i/3], memo[i-1]) + 1;
-            }else if(i % 2 == 0){
-                memo[i] = Math.min(memo[i/2], memo[i-1]) + 1;
-            }else{
-                memo[i] = memo[i-1] + 1;
+        for (int i = 4; i <= N; i++) {
+            if (i % 6 == 0) {
+                memo[i] = Math.min(Math.min(memo[i / 2], memo[i / 3]), memo[i - 1]) + 1;
+            } else if (i % 3 == 0) {
+                memo[i] = Math.min(memo[i / 3], memo[i - 1]) + 1;
+            } else if (i % 2 == 0) {
+                memo[i] = Math.min(memo[i / 2], memo[i - 1]) + 1;
+            } else {
+                memo[i] = memo[i - 1] + 1;
             }
         }
 
         System.out.println(memo[N]);
     }
-}
 
+
+}
