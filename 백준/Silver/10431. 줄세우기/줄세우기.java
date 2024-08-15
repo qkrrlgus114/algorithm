12f0408b1f690e8fadba113d36ab95a30ce6d069
PARK GIHYEON <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-15 14:50:51 +0900

    [Silver V] Title: 줄세우기, Time: 200 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -2,30 +2,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
+
+/*
+ * 자기 앞ㅇ*/
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        int T = Integer.parseInt(input);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int t=0; t<T; t++){
-            String[] inputs = br.readLine().split(" ");
-            int count = 0;
+        int T = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
 
-            for(int i=1; i<inputs.length; i++){
-                for(int j=1; j < i; j++){
-                    if(Integer.parseInt(inputs[j]) > Integer.parseInt(inputs[i])){
-                        count++;
+        for (int t = 0; t < T; t++) {
+            String[] sa = bf.readLine().split(" ");
+            List<Integer> line = new ArrayList<>();
+            sb.append(sa[0]).append(" ");
+
+            int answer = 0;
+            for (int i = 1; i < sa.length; i++) {
+                int num = Integer.parseInt(sa[i]);
+                line.add(num);
+                Collections.sort(line);
+
+                // 몇 번째 서있는지 확인하기
+                for (int j = 0; j < line.size(); j++) {
+                    if(line.get(j) == num){
+                        answer += line.size() - (j + 1);
+                        break;
                     }
                 }
             }
-            System.out.println(inputs[0] + " " + count);
+
+            sb.append(answer).append("\n");
         }
+
+        System.out.println(sb);
     }
-
-
 }
