6e62416b2d4b505bf83b4237f254d00bd8c59ca7
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-17 21:41:05 +0900

    [Bronze I] Title: 유레카 이론, Time: 172 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,51 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    static List<Integer> list;
-    static boolean[] isEureka = new boolean[1001];
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String t = bf.readLine();
-        int T = Integer.parseInt(t);
-        list = new ArrayList<>();
 
-        // 삼각수 리스트 구함
-        getTriangleNum();
-        // 미리 유레카 넘버 체크
-        for(int i=0; i<list.size(); i++){
-            for(int j=0; j<list.size(); j++){
-                for(int k=0; k<list.size(); k++){
-                    int sum = list.get(i) + list.get(j) + list.get(k);
-                    if(sum <= 1000) isEureka[sum] = true;
+        int T = Integer.parseInt(bf.readLine());
+
+        // 삼각수
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=50; i++){
+            list.add(i * (i + 1) / 2);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int t=0; t<T; t++){
+            int n = Integer.parseInt(bf.readLine());
+            boolean check = false; // 가능한지 여부
+
+            outer: for(int i=0; i<list.size(); i++){
+                for(int j=0; j<list.size(); j++){
+                    for(int k=0; k<list.size(); k++){
+                        if(list.get(i) + list.get(j) + list.get(k) == n){
+                            sb.append(1).append("\n");
+                            check = true;
+                            break outer;
+                        }
+                    }
                 }
             }
+
+            if(!check) sb.append(0).append("\n");
         }
 
-        for(int q=0; q<T; q++){
-            String s = bf.readLine();
-            int N = Integer.parseInt(s);
-            if(isEureka[N]) System.out.println(1);
-            else System.out.println(0);
-        }
+        System.out.println(sb);
     }
-
-    // 삼각수의 리스트를 구하는 메서드
-    public static void getTriangleNum(){
-        int value = 1;
-        while(true){
-            int n = value * (value + 1) / 2;
-            if(n > 1000) break;
-            list.add(n);
-            value++;
-        }
-    }
-
-
-
 }
