31fc8feeef1cb9f8472ce3ae68967bc27ed0fc6e
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-17 22:11:38 +0900

    [Silver V] Title: 회문인 수, Time: 128 ms, Memory: 15788 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -1,50 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
+    /*
+     * 1. 그 자체가 회문인지 확인
+     * 2. 2 ~ 64진법까지 노가다 뛰면서 가능한지 판단.
+     * */
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int T = Integer.parseInt(s);
 
-        for(int t=0; t<T; t++){
-            StringBuilder sb = new StringBuilder();
-            s = bf.readLine();
-            boolean check = false;
-            int N = Integer.parseInt(s);
-            sb.append(N);
-            if(s.contentEquals(sb.reverse())){
-                check = true;
-            }else{
-                for(int i=2; i<=64; i++){
-                    sb.setLength(0);
-                    String s1 = binaryChange(N, i);
-                    sb.append(s1);
-                    if(s1.contentEquals(sb.reverse())) {
-                        check = true;
-                        break;
-                    }
-                }
+        int T = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringBuilder checkSb = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(bf.readLine());
+
+            checkSb.append(N).reverse();
+            if (N == Integer.parseInt(checkSb.toString())) {
+                sb.append(1).append("\n");
+            } else {
+                if(checkBinary(N)) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
             }
-            System.out.println(check ? 1 : 0);
+
+            checkSb.setLength(0);
         }
 
+        System.out.println(sb);
     }
 
-    public static String binaryChange(int N, int M){
-        String result = "";
-        while(N != 0){
-            int temp = N % M;
-            N /= M;
-            if(temp < 10) result += temp;
-            else result += (char)(temp - 10 + 'A');
+    public static boolean checkBinary(int N) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= 64; i++) {
+            int M = N;
+            while (M != 0) {
+                int temp = M % i;
+                if (temp < 10) sb.append(temp);
+                else {
+                    sb.append((char) (temp - 10 + 'A'));
+                }
+                M /= i;
+            }
+            String a = sb.toString();
+            String b = sb.reverse().toString();
+            if(a.equals(b)) return true;
+            sb.setLength(0);
         }
 
-        return result;
+        return false;
     }
-
-
 }
