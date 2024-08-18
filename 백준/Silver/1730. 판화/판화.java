f71e9832fe755ac180df32aa6b2024bd0c0b8033
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-18 18:49:08 +0900

    [Silver IV] Title: 판화, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.java
+++ 백준/Silver/1730. 판화/판화.java
@@ -1,38 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-    // 상 하 좌 우(U, D, L ,R)
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+
+    /*
+    * 1. 현재 위치랑 다음 위치를 같이 업데이트한다.
+    * 2.
+    * */
+
     static char[][] graph;
-    static int N;
-    static int cur_y = 0;
-    static int cur_x = 0;
+    static int y = 0;
+    static int x = 0;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        N = Integer.parseInt(s);
+
+        int N = Integer.parseInt(bf.readLine());
         graph = new char[N][N];
-        // 초기화
+        char[] commands = bf.readLine().toCharArray();
+
         for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                graph[i][j] = '.';
-            }
+            Arrays.fill(graph[i], (char) 46);
         }
 
-        // 명령
-        s = bf.readLine();
-
-        for(int z=0; z<s.length(); z++){
-            char temp = s.charAt(z);
-            if(temp == 'U') move(0);
-            else if(temp == 'D') move(1);
-            else if(temp == 'L') move(2);
-            else if(temp == 'R') move(3);
+        for(int i=0; i<commands.length; i++){
+            if(commands[i] == 'D' && y + 1 < N){
+                moveY();
+                y++;
+                moveY();
+            }else if(commands[i] == 'U' && y - 1 >= 0){
+                moveY();
+                y--;
+                moveY();
+            }else if(commands[i] == 'L' && x - 1 >= 0){
+                moveX();
+                x--;
+                moveX();
+            }else if(commands[i] == 'R' && x + 1 < N){
+                moveX();
+                x++;
+                moveX();
+            }
         }
 
         StringBuilder sb = new StringBuilder();
@@ -42,43 +53,25 @@ public class Main {
             }
             sb.append("\n");
         }
+
         System.out.println(sb);
+
     }
 
-    // 팔 이동
-    public static void move(int dir){
-        int ndy = cur_y + dy[dir];
-        int ndx = cur_x + dx[dir];
-        if(ndy < 0 || ndx < 0 || ndy >= N || ndx >= N) return;
-
-        // 이전 위치와 이동 위치에 똑같은 dir로 그음
-        // 위 아래인 경우
-        if(dir == 0 || dir == 1){
-            if(graph[cur_y][cur_x] == '-') {
-                graph[cur_y][cur_x] = '+';
-            }else if(graph[cur_y][cur_x] == '.'){
-                graph[cur_y][cur_x] = '|';
-            }
-            if(graph[ndy][ndx] == '.'){
-                graph[ndy][ndx] = '|';
-            }else if(graph[ndy][ndx] == '-'){
-                graph[ndy][ndx] = '+';
-            }
+    public static void moveY(){
+        if(graph[y][x] == '-' || graph[y][x] == '+'){
+            graph[y][x] = '+';
+        }else{
+            graph[y][x] = '|';
         }
-        // 왼쪽 오른쪽인 경우
-        else{
-            if(graph[cur_y][cur_x] == '|') {
-                graph[cur_y][cur_x] = '+';
-            }else if(graph[cur_y][cur_x] == '.'){
-                graph[cur_y][cur_x] = '-';
-            }
-            if(graph[ndy][ndx] == '.'){
-                graph[ndy][ndx] = '-';
-            }else if(graph[ndy][ndx] == '|'){
-                graph[ndy][ndx] = '+';
-            }
-        }
-        cur_y = ndy;
-        cur_x = ndx;
     }
+
+    public static void moveX(){
+        if(graph[y][x] == '|' || graph[y][x] == '+'){
+            graph[y][x] = '+';
+        }else{
+            graph[y][x] = '-';
+        }
+    }
+
 }
