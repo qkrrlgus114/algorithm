17be9d190666464668c649a9f2e6214f0dfc9d85
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-24 17:09:31 +0900

    [Silver II] Title: 좌표 압축, Time: 2096 ms, Memory: 314676 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -3,41 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
 
-        List<Integer> ori = new ArrayList<>();
+        int N = Integer.parseInt(bf.readLine());
+        Set<Integer> set = new HashSet<>();
         List<Integer> list = new ArrayList<>();
+
         String[] sa = bf.readLine().split(" ");
-        for(String temp : sa){
-            ori.add(Integer.parseInt(temp));
-            list.add(Integer.parseInt(temp));
+        for(int i=0; i<N; i++){
+            int n = Integer.parseInt(sa[i]);
+            set.add(n);
+            list.add(n);
         }
 
-        Collections.sort(list);
+        List<Integer> sortList = new ArrayList<>(set);
+        Collections.sort(sortList);
+
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(list.get(0), 0);
-
-        for(int i=1; i<list.size(); i++){
-            if(!list.get(i).equals(list.get(i-1))){
-                int temp = map.get(list.get(i-1));
-                map.put(list.get(i), temp + 1);
-            }else{
-                int temp = map.get(list.get(i-1));
-                map.put(list.get(i), temp);
-            }
+        int index = 0;
+        for(Integer num : sortList){
+            map.put(num, index++);
         }
+
+        List<Integer> result = new ArrayList<>();
+        for(Integer num : list){
+            result.add(map.get(num));
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        for(Integer i : ori){
-            sb.append(map.get(i)).append(" ");
+        for(Integer num : result){
+            sb.append(num).append(" ");
         }
-
+        
         System.out.println(sb);
     }
 }
