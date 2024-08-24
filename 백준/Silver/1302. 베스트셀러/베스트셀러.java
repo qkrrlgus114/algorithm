2639a9cf4f1379dbbd08561995a8fe0996cbbba1
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-24 16:09:52 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -3,39 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
+
+        int N = Integer.parseInt(bf.readLine());
 
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<N; i++){
-            s = bf.readLine();
-            if(map.containsKey(s)){
-                int temp = map.get(s);
-                map.put(s, temp + 1);
-            }else{
-                map.put(s, 1);
+
+        for (int i = 0; i < N; i++) {
+            String book = bf.readLine();
+            map.put(book, map.getOrDefault(book, 0) + 1);
+        }
+
+        String name = "";
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > count) {
+                name = entry.getKey();
+                count = entry.getValue();
+            } else if (count == entry.getValue() && entry.getKey().compareTo(name) < 0) {
+                name = entry.getKey();
             }
         }
 
-        int max_count = 0;
-        String result = "";
-
-        for(Map.Entry<String, Integer> m : map.entrySet()){
-            int value = m.getValue();
-            String title = m.getKey();
-            if(value > max_count){
-                max_count = value;
-                result = title;
-            }else if(value == max_count && result.compareTo(title) > 0){
-                result = title;
-            }
-        }
-
-        System.out.println(result);
+        System.out.println(name);
     }
 }
