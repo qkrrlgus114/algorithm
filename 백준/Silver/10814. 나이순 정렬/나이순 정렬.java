8354476412ca9f603b9677a587224a2d385add8a
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-24 15:36:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 648 ms, Memory: 55972 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,62 +3,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Human implements Comparable<Human>{
-    private int age;
-    private String name;
-    private int index;
-
-    public Human(int age, String name, int index){
-        this.age = age;
-        this.name = name;
-        this.index = index;
-    }
-
-    public int compareTo(Human h){
-        if(age == h.age) {
-            return index - h.index;
-        }
-        return age - h.age;
-    }
-
-    public int getAge(){
-        return this.age;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-}
-
 public class Main {
-
-    /*
-    * 1. 나이 오름차순
-    * 2. 가입한 순서
-    * */
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
 
-        List<Human> list = new ArrayList<>();
+        int N = Integer.parseInt(bf.readLine());
+        List<Person> person = new ArrayList<>();
 
-        int index = 0;
-
+        int index = 1;
         for(int i=0; i<N; i++){
             String[] sa = bf.readLine().split(" ");
-            list.add(new Human(Integer.parseInt(sa[0]), sa[1], index++));
+            person.add(new Person(index++, Integer.parseInt(sa[0]), sa[1]));
         }
 
-        Collections.sort(list);
+        Collections.sort(person, new Comparator<Person>(){
+            public int compare(Person o1, Person o2){
+                if(o1.age == o2.age){
+                    return o1.index - o2.index;
+                }
+                return o1.age - o2.age;
+            }
+        });
 
         StringBuilder sb = new StringBuilder();
-        for(Human h : list){
-            sb.append(h.getAge()).append(" ").append(h.getName()).append("\n");
+        for(Person per : person){
+            sb.append(per.age).append(" ").append(per.name).append("\n");
         }
 
         System.out.println(sb);
+    }
 
+}
+
+class Person{
+    int index;
+    int age;
+    String name;
+
+    public Person(int index, int age, String name){
+        this.index = index;
+        this.age = age;
+        this.name = name;
     }
 }
