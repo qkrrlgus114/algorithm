2d5c7a388a610cea2793b6d2fd89fe53af55f456
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-28 20:03:35 +0900

    [Silver II] Title: Generic Queries, Time: 1328 ms, Memory: 307484 KB -BaekjoonHub

--- 백준/Silver/16713. Generic Queries/Generic Queries.java
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -1,55 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
 import java.util.*;
-
-class Cop{
-    private int start;
-    private int end;
-
-    public Cop(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public int getStart(){
-        return this.start;
-    }
-
-    public int getEnd(){
-        return this.end;
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
-        int[] arr = new int[N+1];
-        int[] sum = new int[N+1];
+
+        int[] ori = new int[N + 1];
+        int[] prefix = new int[N + 1];
+
         sa = bf.readLine().split(" ");
-        for(int i=1; i<=N; i++){
-            arr[i] = Integer.parseInt(sa[i-1]);
+
+        for (int i = 0; i < N; i++) {
+            ori[i + 1] = Integer.parseInt(sa[i]);
         }
-        
-        // 구간합
-        for(int i=1; i<=N; i++){
-            sum[i] = sum[i-1] ^ arr[i];
+
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] ^ ori[i];
         }
 
         int result = 0;
 
-        for(int i=0; i<M; i++) {
+        for (int i = 0; i < M; i++) {
             sa = bf.readLine().split(" ");
             int start = Integer.parseInt(sa[0]);
             int end = Integer.parseInt(sa[1]);
-            result ^= sum[end] ^ sum[start-1];
+
+            result ^= prefix[end] ^ prefix[start - 1];
         }
 
         System.out.println(result);
-
     }
 }
