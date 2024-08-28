7f0e9f259815a8fc8e3b129d4417c31251c334ac
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-08-28 19:52:58 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 620 ms, Memory: 60452 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,54 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
 import java.util.*;
-
-class Cop{
-    private int start;
-    private int end;
-
-    public Cop(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public int getStart(){
-        return this.start;
-    }
-
-    public int getEnd(){
-        return this.end;
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
-        StringBuilder sb = new StringBuilder();
 
-        int[] arr = new int[N];
-        int[] sum = new int[N+1];
+        int[] ori = new int[N + 1];
+        int[] prefix = new int[N + 1];
 
         sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(sa[i]);
+
+        for (int i = 0; i < N; i++) {
+            ori[i + 1] = Integer.parseInt(sa[i]);
         }
 
-        // 누적합 구하기
-        for(int i=1; i<N+1; i++){
-            sum[i] = sum[i-1] + arr[i-1];
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] + ori[i];
         }
 
-        for(int i=0; i<M; i++){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
             sa = bf.readLine().split(" ");
             int start = Integer.parseInt(sa[0]);
             int end = Integer.parseInt(sa[1]);
-            int result = sum[end] - sum[start - 1];
-            sb.append(result).append("\n");
+
+            sb.append(prefix[end] - prefix[start - 1]).append("\n");
         }
 
         System.out.println(sb);
