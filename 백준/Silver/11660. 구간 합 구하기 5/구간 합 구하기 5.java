b59211028fddb768b443bf7d8bc6dd0a0ffeffc5
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-09-01 18:15:21 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 848 ms, Memory: 148804 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,12 +1,7 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 
 public class Main {
 
@@ -17,37 +12,34 @@ public class Main {
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
 
-        int[][] arr = new int[N+1][N+1];
-        // 입력 받기
-        for(int i=1; i<=N; i++){
+        int[][] ori = new int[N+1][N+1];
+        for(int i=0; i<N; i++){
             sa = bf.readLine().split(" ");
-            for(int j=1; j<=N; j++){
-                arr[i][j] = Integer.parseInt(sa[j-1]);
+            for(int j=0; j<N; j++){
+                ori[i+1][j+1] = Integer.parseInt(sa[j]);
             }
         }
 
-        // 누적합 구하기
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                arr[i][j] = arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1] + arr[i][j];
+        int[][] prefix = new int[N+1][N+1];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                prefix[i+1][j+1] = prefix[i+1][j] + prefix[i][j+1] + ori[i+1][j+1] - prefix[i][j];
             }
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        for(int q=0; q<M; q++){
+
+        for(int i=0; i<M; i++){
             sa = bf.readLine().split(" ");
             int x1 = Integer.parseInt(sa[0]);
             int y1 = Integer.parseInt(sa[1]);
             int x2 = Integer.parseInt(sa[2]);
             int y2 = Integer.parseInt(sa[3]);
 
-            int value = arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1];
-            sb.append(value).append("\n");
+            int sum = prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1];
+            sb.append(sum).append("\n");
         }
 
         System.out.println(sb);
-
-        
     }
 }
-
