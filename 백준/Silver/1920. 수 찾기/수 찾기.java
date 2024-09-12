1163e60c2359c141b780172a72443f17681e8a7b
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-09-12 22:40:13 +0900

    [Silver IV] Title: 수 찾기, Time: 568 ms, Memory: 52016 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,61 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int[] targetArr;
-    static int[] searchArr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
+
+        int N = Integer.parseInt(bf.readLine());
+        Set<Integer> set = new HashSet<>();
+
         String[] sa = bf.readLine().split(" ");
-        targetArr = new int[N];
         for(int i=0; i<N; i++){
-            targetArr[i] = Integer.parseInt(sa[i]);
+            set.add(Integer.parseInt(sa[i]));
         }
-        s = bf.readLine();
-        int M = Integer.parseInt(s);
+
+        int M = Integer.parseInt(bf.readLine());
         sa = bf.readLine().split(" ");
-        searchArr = new int[M];
+
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++){
-            searchArr[i] = Integer.parseInt(sa[i]);
+            if(set.contains(Integer.parseInt(sa[i]))) sb.append("1").append("\n");
+            else sb.append("0").append("\n");
         }
 
-
-        // 이분 탐색 진행
-        Arrays.sort(targetArr);
-
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<M; i++){
-            boolean value = binarySearch(0, N-1, searchArr[i]);
-            if(value) result.add(1);
-            else result.add(0);
-        }
-
-        for(Integer i : result){
-            System.out.println(i);
-        }
-
-    }
-
-    public static boolean binarySearch(int start, int end, int target){
-        if(start > end){
-            return false;
-        }
-
-        int mid = (start + end) / 2;
-        int value = targetArr[mid];
-
-        if(target == value) return true;
-        else if(target > value){
-            return binarySearch(mid + 1, end, target);
-        }else{
-            return binarySearch(start, mid - 1, target);
-        }
+        System.out.println(sb);
     }
 }
