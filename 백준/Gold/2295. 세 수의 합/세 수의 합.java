a15570488be293d2e043a98fbd82b8fe7255b5f0
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-09-13 23:47:54 +0900

    [Gold IV] Title: 세 수의 합, Time: 760 ms, Memory: 34824 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,72 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int N;
     static int[] arr;
-    static int max = Integer.MIN_VALUE;
-    static List<Integer> sum = new ArrayList<>();
+    static List<Integer> prefix = new ArrayList<>();
+    static int N;
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bf.readLine();
-        N = Integer.parseInt(s);
+        N = Integer.parseInt(bf.readLine());
         arr = new int[N];
 
-        for(int i=0; i<N; i++) {
-            s = bf.readLine();
-            arr[i] = Integer.parseInt(s);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=i; j<N; j++){
-                sum.add(arr[i] + arr[j]);
-            }
-        }
-        Collections.sort(sum);
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                int value = arr[i] - arr[j];
-                boolean result = binarySearch(0, sum.size() - 1, value);
-                if(result){
-                    max = Math.max(max, arr[i]);
-                }
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                prefix.add(arr[i] + arr[j]);
             }
         }
 
-        System.out.println(max);
+        Collections.sort(prefix);
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                boolean check = binarySearch(0, prefix.size() - 1, arr[i] - arr[j]);
+                if(check) result = Math.max(result, arr[i]);
+            }
+        }
+
+        System.out.println(result);
     }
 
-    public static boolean binarySearch(int low, int high, int value){
-        if(low > high){
-            return false;
-        }
+    public static boolean binarySearch(int start, int end, int target) {
+        if (start > end) return false;
 
-        int mid = (low + high) / 2;
+        int mid = (start + end) / 2;
+        int value = prefix.get(mid);
 
-        // 값이 왼쪽에 있으면
-        if(sum.get(mid) > value){
-            return binarySearch(low, mid-1, value);
-        }
-
-        // 값이 오른쪽에 있으면
-        if(sum.get(mid) < value){
-            return binarySearch(mid+1, high, value);
-        }
-
-        if(sum.get(mid) == value){
+        if (value == target) {
             return true;
+        } else if (value > target) {
+            return binarySearch(start, mid - 1, target);
+        } else {
+            return binarySearch(mid + 1, end, target);
         }
-
-        return false;
     }
 }
