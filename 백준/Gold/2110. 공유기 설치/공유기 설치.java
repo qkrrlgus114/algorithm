3a8c199adf82d292ddc391a28aa3d7eab1174697
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-09-22 14:23:26 +0900

    [Gold IV] Title: 공유기 설치, Time: 276 ms, Memory: 28724 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,57 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static Long[] house;
-    static int N;
-    static int C;
-    static Long result = Long.MIN_VALUE;
+    static int N, C;
+    static int[] positions;
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
 
+        String[] sa = bf.readLine().split(" ");
         N = Integer.parseInt(sa[0]);
         C = Integer.parseInt(sa[1]);
 
-        house = new Long[N];
+        positions = new int[N];
+
         for(int i=0; i<N; i++){
-            String s = bf.readLine();
-            house[i] = Long.parseLong(s);
+            positions[i] = Integer.parseInt(bf.readLine());
         }
+        Arrays.sort(positions);
 
-        Arrays.sort(house);
-
-        binarySearch(1L, house[N-1]);
+        binarySearch(1, 1000000000);
         System.out.println(result);
     }
 
-    public static void binarySearch(Long low, Long high){
-        if(low > high) return;
+    public static void binarySearch(int l, int r){
+        while(l <= r){
+            // 공유기 설치 개수
+            int count = 0;
+            int mid = (l + r) / 2;
+            // 이전 설치 좌표
+            int position = 0;
 
-        Long mid = (low + high) / 2;
+            for(int i=0; i<N; i++){
+                // 공유기를 전부 채웠으면 종료
+                if(count == C) break;
 
-        int count = 1;
-        int house_index = 0;
-        for(int i=1; i<house.length; i++){
-            if(house[i] - house[house_index] >= mid){
-                count++;
-                house_index = i;
+                if(position == 0){
+                    count++;
+                    position = positions[i];
+                }else{
+                    if(positions[i] - position >= mid){
+                        count++;
+                        position = positions[i];
+                    }
+                }
+            }
+
+            if(C == count){
+                result = Math.max(result, mid);
+                l = mid + 1;
+            }else if(C > count){
+                r = mid - 1;
             }
         }
-
-        // 설치한 공유기의 개수가 적으면
-        if(count < C){
-            binarySearch(low, mid-1);
-        }else{
-            if(result < mid) result = mid;
-            binarySearch(mid+1, high);
-        }
     }
-
-
 
 }
