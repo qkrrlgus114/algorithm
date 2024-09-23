9448fda2972b2958fd0db1320b85e5987c8b6f4f
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-09-23 23:00:52 +0900

    [Silver IV] Title: 수들의 합 2, Time: 124 ms, Memory: 15776 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,47 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] sa = bf.readLine().split(" ");
-        int N = Integer.parseInt(sa[0]);
-        int M = Integer.parseInt(sa[1]);
+		String[] sa = bf.readLine().split(" ");
 
-        int[] arr = new int[N];
-        sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(sa[i]);
-        }
+		int N = Integer.parseInt(sa[0]);
+		int M = Integer.parseInt(sa[1]);
 
-        int l = 0;
-        int r = 0;
-        int sum = 0;
-        int count = 0;
+		int[] arr = new int[N];
+		sa = bf.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(sa[i]);
+		}
 
-        while(true){
-            if(r > N) break;
+		int l = 0;
+		int r = 0;
+		int sum = arr[0];
+		int cnt = 0;
 
-            if(sum == M){
-                count++;
-                l++;
-                sum -= arr[l-1];
-            }else if(sum < M){
+		while (l != N && r != N) {
+			if (sum == M) {
+				cnt++;
+				sum -= arr[l];
+				l++;
+			} else if (sum > M) {
+				sum -= arr[l];
+				l++;
+			} else {
                 r++;
-                if(r > N) break;
-                sum += arr[r-1];
-            }else if(sum > M){
-                l++;
-                sum -= arr[l-1];
-            }
-        }
-        System.out.println(count);
+                if(r != N){
+                    sum += arr[r];
+                }
+			}
+		}
 
-    }
+		System.out.println(cnt);
+
+	}
+
 }
