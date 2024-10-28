439cda4e56e5416afe82218527e5b1fbc25771b5
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-10-28 19:39:31 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,38 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    /*
-    * 앞 사람이 최대한 빨리 끝나는 게 좋음.
-    * 결국 정렬을 해서 계산하면 됨
-    * */
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
+		int N = Integer.parseInt(bf.readLine());
+		String[] sa = bf.readLine().split(" ");
 
-        String[] sa = bf.readLine().split(" ");
-        int[] arr = new int[N];
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(sa[i]);
-        }
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(sa[i]);
+		}
 
-        Arrays.sort(arr);
+		Arrays.sort(arr);
 
-        int sum = 0;
-        int result = 0;
-        for(int i=0; i<N; i++){
-            sum += arr[i];
-            result += sum;
-        }
+		int sum = 0;
+		int result = 0;
 
-        System.out.println(result);
-    }
+		for(int i=0; i<N; i++){
+			sum += arr[i];
+			result += sum;
+		}
+
+		System.out.println(result);
+
+	}
 }
-
-
-
