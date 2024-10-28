ef827fbf00e0d0b008014b2b2a108796dccfbb6d
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-10-28 19:46:39 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 620 ms, Memory: 63912 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,32 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
-        int N = Integer.parseInt(sa[0]);
-        int M = Integer.parseInt(sa[1]);
-        Map<String, String> siteAndPass = new HashMap<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i=0; i<N; i++){
-            sa = bf.readLine().split(" ");
-            String site = sa[0];
-            String pass = sa[1];
+		String[] sa = bf.readLine().split(" ");
+		int N = Integer.parseInt(sa[0]);
+		int M = Integer.parseInt(sa[1]);
 
-            siteAndPass.put(site, pass);
-        }
+		Map<String, String> map = new HashMap<>();
 
-        StringBuilder sb = new StringBuilder();
-        for(int j=0; j<M; j++){
-            sb.append(siteAndPass.get(bf.readLine())).append("\n");
-        }
+		for(int i=0; i<N; i++){
+			sa = bf.readLine().split(" ");
 
-        System.out.println(sb);
-    }
+			map.put(sa[0], sa[1]);
+		}
 
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<M; i++){
+			String url = bf.readLine();
+
+			sb.append(map.get(url)).append("\n");
+		}
+
+		System.out.println(sb);
+	}
 }
