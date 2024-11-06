f8ff136f71485e9ebe19bfc30efe0c9f3c50f869
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-06 21:16:48 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 26268 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,26 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bf.readLine());
-        StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(bf.readLine());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0; i<N; i++){
-            int x = Integer.parseInt(bf.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
 
-            if(x == 0){
-                if(pq.isEmpty()) sb.append(0).append("\n");
-                else sb.append(pq.poll()).append("\n");
-            }else{
-                pq.add(x);
-            }
-        }
+		for(int i=0; i<N; i++){
+			int M = Integer.parseInt(bf.readLine());
+			if(M == 0){
+				if(pq.isEmpty()) sb.append("0").append("\n");
+				else sb.append(pq.poll()).append("\n");
+			}else{
+				pq.add(M);
+			}
+		}
 
-        System.out.println(sb);
-    }
+		System.out.println(sb);
+	}
+
 }
