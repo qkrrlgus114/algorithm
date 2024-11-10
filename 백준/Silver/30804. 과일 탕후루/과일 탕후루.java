848183e8ea3226a115c26d64fad04e4a43147e83
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-10 22:13:49 +0900

    [Silver II] Title: 과일 탕후루, Time: 300 ms, Memory: 32772 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -4,56 +4,50 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int[] fruit = new int[10];
-    static int[] arr;
-    static int max = Integer.MIN_VALUE;
-    static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String n = bf.readLine();
-        N = Integer.parseInt(n);
+		int N = Integer.parseInt(bf.readLine());
+		String[] sa = bf.readLine().split(" ");
 
-        arr = new int[N];
-        String[] sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(sa[i]);
-        }
+		int[] check = new int[10];
+		int[] arr = new int[N];
 
-        two(0, 0, 0, 0);
-        System.out.println(max);
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(sa[i]);
+		}
 
-    }
+		int l = 0;
+		int r = 0;
+		int result = 1; // 탕후루 최대 개수
+		int type = 1; // 탕후루 종류 개수
+		check[arr[r]]++;
 
-    public static void two(int left, int right, int count, int type){
-        if(left > right || right >= N){
-            return;
-        }
+		while (r < N - 1) {
+			r++;
+			check[arr[r]]++;
+			if(check[arr[r]] != 1){
+				result = Math.max(result, r - l + 1);
+			}else{
+				type++;
+				if(type <= 2){
+					result = Math.max(result, r - l + 1);
+				}else{
+					while(l < r){
+						check[arr[l]]--;
+						if(check[arr[l]] == 0){
+							type--;
+							l++;
+							break;
+						}
+						l++;
+					}
+				}
+			}
+		}
 
-        // 만약에 right 지점의 과일이 아직 0개라면
-        if(fruit[arr[right]] == 0){
-            // 종류 + 1
-            type++;
-        }
-        // 총 개수 + 1
-        count++;
-        // 해당 과일 + 1
-        fruit[arr[right]]++;
+		System.out.println(result);
 
-        // 만약에 과일 종류가 3개 이상이라면
-        while(type >= 3){
-            // 왼쪽 포인터에 있는 과일, 개수, 종류 제거
-            fruit[arr[left]]--;
-            if(fruit[arr[left]] == 0){
-                type--;
-            }
-            count--;
-            left++;
-        }
+	}
 
-        // 최대값 비교
-        max = Math.max(max, count);
-
-        two(left, right + 1, count, type);
-    }
 }
