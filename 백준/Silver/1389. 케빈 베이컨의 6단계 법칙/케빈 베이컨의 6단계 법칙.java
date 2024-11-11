afad3b933906a83a2de08817aa8f3ec0dae0da3d
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-11 19:30:01 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 116 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,69 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
 
-    static int N;
-    // 가장 작은 케빈 베이컨
-    static int result = Integer.MAX_VALUE;
-    // 가장 작은 케빈 베이컨을 가진 사람
-    static int result_index = Integer.MAX_VALUE;
-    static List<Integer>[] graph;
+	static int N, M;
+	static int result = Integer.MAX_VALUE;
+	static int humanIdx = Integer.MAX_VALUE;
+	static List<Integer>[] graph;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = bf.readLine().split(" ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String[] sa = bf.readLine().split(" ");
 
-        N = Integer.parseInt(inputs[0]);
-        int M = Integer.parseInt(inputs[1]);
+		N = Integer.parseInt(sa[0]);
+		M = Integer.parseInt(sa[1]);
 
-        graph = new List[N +1 ];
-        for(int i=0; i<graph.length; i++){
-            graph[i] = new ArrayList<>();
-        }
+		graph = new List[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			sa = bf.readLine().split(" ");
+			int start = Integer.parseInt(sa[0]);
+			int end = Integer.parseInt(sa[1]);
 
-        for(int i=0; i<M; i++){
-            inputs = bf.readLine().split(" ");
-            int start = Integer.parseInt(inputs[0]);
-            int end = Integer.parseInt(inputs[1]);
+			graph[start].add(end);
+			graph[end].add(start);
+		}
 
-            graph[start].add(end);
-            graph[end].add(start);
-        }
+		for (int i = 1; i <= N; i++) {
+			bfs(i);
+		}
 
-        for(int i=1; i<=N; i++){
-            bfs(i);
-        }
+		System.out.println(humanIdx);
+	}
 
-        System.out.println(result_index);
+	public static void bfs(int y) {
+		int sum = 0;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {y, 0});
+		boolean[] visited = new boolean[N + 1];
+		visited[y] = true;
 
-    }
+		while (!q.isEmpty()) {
+			int[] temp = q.poll();
+			int cur = temp[0];
+			int step = temp[1];
 
-    public static void bfs(int t){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{t, 0});
-        boolean[] visited = new boolean[N+1];
-        visited[t] = true;
-        int sum = 0;
+			for (int i = 0; i < graph[cur].size(); i++) {
+				int next = graph[cur].get(i);
+				if (visited[next])
+					continue;
 
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            int index = temp[0];
-            int count = temp[1];
-            for(int i=0; i<graph[index].size(); i++){
-                int next = graph[index].get(i);
-                if(visited[next]) continue;
-                visited[next] = true;
-                sum += count + 1;
-                q.add(new int[]{next, count + 1});
-            }
-        }
+				visited[next] = true;
+				sum += step + 1;
+				q.add(new int[] {next, step + 1});
+			}
+		}
 
-        if(result > sum){
-            result_index = t;
-            result = sum;
-        }
-    }
+		if (result == sum) {
+			humanIdx = Math.min(humanIdx, y);
+		} else if (result > sum) {
+			result = sum;
+			humanIdx = y;
+		}
+	}
+
 }
