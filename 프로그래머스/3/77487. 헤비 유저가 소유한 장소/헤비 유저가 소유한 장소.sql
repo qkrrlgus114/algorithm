66437fafc0f3d92535272ff8d228ee0b02e6dc7b
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-18 23:30:06 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,11 +1,13 @@
-# 공간을 둘 이상 등록한 사람
-# 공간의 정보를 아이디 순으로
+# '헤비 유저' -> 공간을 둘 이상 등록한 사람
+# 헤비 유저가 등록한 공간의 정보
+# ID ASC
 
-select *
-from places
-where host_id in (select host_id
-    from places
-    group by host_id 
-    having count(host_id) >= 2)
-order by id asc
 
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN(
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(HOST_ID) >= 2)
+ORDER BY ID ASC
