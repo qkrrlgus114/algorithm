159acf350e76534115e920879733907eb82e4f09
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-19 23:46:17 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 116 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -1,43 +1,48 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(sa[0]);
-        int K = Integer.parseInt(sa[1]);
-        int B = Integer.parseInt(sa[2]);
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
-        }
+		String[] sa = bf.readLine().split(" ");
+		int N = Integer.parseInt(sa[0]);
+		int K = Integer.parseInt(sa[1]);
+		int B = Integer.parseInt(sa[2]);
 
-        for (int i = 0; i < B; i++) {
-            String s = bf.readLine();
-            arr[Integer.parseInt(s) - 1] = 0;
-        }
+		boolean[] arr = new boolean[N];
+		int result = 0;
 
-        int answer = Integer.MAX_VALUE;
+		for (int i = 0; i < B; i++) {
+			arr[Integer.parseInt(bf.readLine()) - 1] = true;
+		}
 
-        int count = 0;
-        for (int i = 0; i <= N - K; i++) {
-            count = 0;
-            for (int j = i; j < i + K; j++) {
-                if (arr[j] == 0) count++;
-            }
-            answer = Math.min(answer, count);
-        }
+		int tempResult = 0;
+		int l = 0;
+		int r = 0;
+		for (r = 0; r < K; r++) {
+			if (arr[r])
+				tempResult++;
+		}
+		r--;
 
-        System.out.println(answer);
-    }
+		result = tempResult;
+		while (r < N - 1) {
+			r++;
+			if (arr[r])
+				tempResult++;
+
+			if (arr[l])
+				tempResult--;
+			l++;
+
+			result = Math.min(result, tempResult);
+		}
+
+		System.out.println(result);
+
+	}
+
 }
-
