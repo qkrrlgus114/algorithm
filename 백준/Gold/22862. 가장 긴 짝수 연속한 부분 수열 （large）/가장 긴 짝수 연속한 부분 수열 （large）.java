666b7c6d3f6b8fbe79f27ebd126424108f8ef54b
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2024-11-28 21:22:02 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 436 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -1,56 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+		int S, K = 0;
+		String[] sa = bf.readLine().split(" ");
+		S = Integer.parseInt(sa[0]);
+		K = Integer.parseInt(sa[1]);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int l = 0;
+		int r = 0;
+		int evenCnt = 0;
+		int result = 0;
 
-        String[] sa = bf.readLine().split(" ");
-        int N = Integer.parseInt(sa[0]);
-        int K = Integer.parseInt(sa[1]);
+		int[] arr = new int[S];
+		sa = bf.readLine().split(" ");
+		for (int i = 0; i < S; i++) {
+			arr[i] = Integer.parseInt(sa[i]);
+		}
 
-        int[] S = new int[N];
-        sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
-            S[i] = Integer.parseInt(sa[i]);
-        }
+		while (r < S) {
+			if (arr[r] % 2 == 0) {
+				evenCnt++;
+				r++;
+				result = Math.max(result, evenCnt);
+			} else {
+				if (K == 0) {
+					while (l < r) {
+						if (arr[l] % 2 == 0) {
+							evenCnt--;
+							l++;
+						} else {
+							K++;
+							l++;
+							break;
+						}
+					}
+				} else {
+					K--;
+					r++;
+				}
+			}
+		}
 
-        int answer = 0;
-        int l = 0;
-        int r = 0;
-        int even = 0;
-        while(r < N){
-            // 짝수라면
-            if(S[r] % 2 == 0){
-                even++;
-                r++;
-                answer = Math.max(answer, even);
-            }
-            // 홀수라면
-            else{
-                if(K == 0){
-                    while(true){
-                        if(S[l] % 2 == 0){
-                            even--;
-                            l++;
-                        }else{
-                            K++;
-                            l++;
-                            break;
-                        }
-                    }
-                }else{
-                    K--;
-                    r++;
-                }
-            }
-        }
+		System.out.println(result);
 
-        System.out.println(answer);
-    }
+	}
+
 }
