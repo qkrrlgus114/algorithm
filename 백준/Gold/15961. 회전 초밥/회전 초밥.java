c4f9bf5a9937cd40c4ac8354bc2e53049fe717f7
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-02-11 00:32:24 +0900

    [Gold IV] Title: 회전 초밥, Time: 1020 ms, Memory: 313028 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,70 +1,77 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
 
+        String[] sa = bf.readLine().split(" ");
+        // 접시의 수
         int N = Integer.parseInt(sa[0]);
+        // 초밥의 가짓수
         int d = Integer.parseInt(sa[1]);
+        // 연속해서 먹는 접시의 수
         int k = Integer.parseInt(sa[2]);
+        // 쿠폰 번호
         int c = Integer.parseInt(sa[3]);
 
-        int[] chobab = new int[N];
-        int[] count = new int[d+1];
-        int answer = 0;
-
-        for(int i=0; i<N; i++){
-            String s = bf.readLine();
-            chobab[i] = Integer.parseInt(s);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
         }
 
-        for(int i=0; i<k; i++){
-            count[chobab[i]]++;
-            if(count[chobab[i]] == 1) answer++;
+        Map<Integer, Integer> map = new HashMap<>();
+        // 처음에 초밥을 k 선택
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
-        int finalAnswer = answer;
+        int s = 0;
+        int e = k - 1;
+        int result = map.containsKey(c) ? map.size() : map.size() + 1;
 
-        int l = 0;
-        int r = k-1;
+        // 구간을 옮기면서 탐색
+        while (e < N - 1) {
+            e++;
+            map.put(arr[e], map.getOrDefault(arr[e], 0) + 1);
 
-        for(int i=0; i<N-1; i++){
-            // 최대 가짓수 갱신
-            if(count[c] == 0){
-                finalAnswer = Math.max(finalAnswer, answer + 1);
-            }else{
-                finalAnswer = Math.max(finalAnswer, answer);
+            if (map.get(arr[s]) == 1) {
+                map.remove(arr[s]);
+            } else {
+                map.put(arr[s], map.get(arr[s]) - 1);
             }
-
-            r++;
-            if(r == N){
-                r = 0;
+            s++;
+            
+            if (!map.containsKey(c)) {
+                result = Math.max(result, map.size() + 1);
+            } else {
+                result = Math.max(result, map.size());
             }
-            count[chobab[r]]++;
-            if(count[chobab[r]] == 1) answer++;
-            l++;
-            count[chobab[l-1]]--;
-            if(count[chobab[l-1]] == 0) answer--;
-
         }
 
-        // 최대 가짓수 갱신
-        if(count[c] == 0){
-            finalAnswer = Math.max(finalAnswer, answer + 1);
-        }else{
-            finalAnswer = Math.max(finalAnswer, answer);
-        }
-        System.out.println(finalAnswer);
+        e = -1;
+        // k - 1번을 더 확인해야함.
+        for (int i = 0; i <= k - 1; i++) {
+            e++;
+            map.put(arr[e], map.getOrDefault(arr[e], 0) + 1);
 
+            if (map.get(arr[s]) == 1) {
+                map.remove(arr[s]);
+            } else {
+                map.put(arr[s], map.get(arr[s]) - 1);
+            }
+            s++;
+
+            if (!map.containsKey(c)) {
+                result = Math.max(result, map.size() + 1);
+            } else {
+                result = Math.max(result, map.size());
+            }
+        }
+        System.out.println(result);
     }
 }
-
