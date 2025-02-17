8846524af7eb889e847bd1cc0c9d716c8d4a6661
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-02-17 21:54:47 +0900

    [Silver III] Title: 게임, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,8 +1,6 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
@@ -10,35 +8,27 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sa = bf.readLine().split(" ");
-
         int X = Integer.parseInt(sa[0]);
         int Y = Integer.parseInt(sa[1]);
-
         int Z = (int) ((long) Y * 100 / X);
 
-        long result = -1;
+        int l = 0;
+        int r = 1000000000;
+        long answer = Long.MAX_VALUE;
 
-        long l = 0;
-        long r = 1000000000;
-
-        while(l <= r){
-            long mid = (l + r) / 2;
-
+        while (l <= r) {
+            int mid = (l + r) / 2;
             int value = (int) ((long) (Y + mid) * 100 / (X + mid));
-
-            if(value > Z){
-                result = mid;
+            if (value > Z) {
                 r = mid - 1;
-            }else{
+                answer = Math.min(answer, mid);
+            } else {
                 l = mid + 1;
             }
         }
 
-        System.out.println(result);
+        if (answer == Long.MAX_VALUE) System.out.println(-1);
+        else System.out.println(answer);
     }
 
-
-
 }
-
-
