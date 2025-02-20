edee08a79982e693ab0d4d3ddc3022e395feb490
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-02-20 21:52:31 +0900

    [Silver I] Title: 보석 상자, Time: 320 ms, Memory: 35708 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -1,54 +1,49 @@
-import javax.security.auth.login.AccountNotFoundException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Main {
 
-    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
 
-        int[] cry = new int[M];
-        int max = 0;
-        for(int i=0; i<M; i++){
-            String s = bf.readLine();
-            int count = Integer.parseInt(s);
-            cry[i] = count;
-            max = Math.max(max, count);
+        int[] arr = new int[M];
+        long e = 0;
+        for (int i = 0; i < M; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+            e = Math.max(e, arr[i]);
         }
 
-        int answer = Integer.MAX_VALUE;
-        int start = 1;
-        int end = max;
+        long s = 1;
+        long answer = Long.MAX_VALUE;
 
-        while(start <= end){
-            int mid = (start + end) / 2;
+        while (s <= e) {
+            long m = (s + e) / 2;
 
-            int sum = 0;
-            for(Integer num : cry){
-                int people = num % mid == 0 ? num / mid : num / mid + 1;
-                sum += people;
+            int cnt = 0;
+            for (int i = 0; i < M; i++) {
+                if (arr[i] % m == 0) {
+                    cnt += arr[i] / m;
+                } else {
+                    cnt += (arr[i] / m) + 1;
+                }
+            }
+            if (cnt <= N) {
+                answer = Math.min(answer, m);
+                e = m - 1;
+            } else {
+                s = m + 1;
             }
 
-            if(sum <= N){
-                answer = Math.min(answer, mid);
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
         }
 
         System.out.println(answer);
     }
-}
 
+
+}
