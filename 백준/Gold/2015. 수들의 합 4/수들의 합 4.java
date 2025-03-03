b214d0ac05957c5776e4265eb4a3e90b4e068384
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-03 20:37:40 +0900

    [Gold IV] Title: 수들의 합 4, Time: 300 ms, Memory: 34764 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -1,40 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
+
         int N = Integer.parseInt(sa[0]);
         int K = Integer.parseInt(sa[1]);
 
-        int[] arr = new int[N+1];
+        int[] arr = new int[N];
         sa = bf.readLine().split(" ");
-        for(int i=1; i<=N; i++){
-            arr[i] = Integer.parseInt(sa[i-1]);
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(sa[i]);
         }
 
-        // 누적합 계산
-        for(int i=1; i<=N; i++){
-            arr[i] = arr[i-1] + arr[i];
+        int[] prefixSum = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-
+        // 누적합 값의 개수를 카운트
+        Map<Integer, Integer> countMap = new HashMap<>();
+        // prefixSum[i] == K인 경우가 생길 수 있기에 0을 미리 하나 더해준다.
+        // prefixSum[i] - K = 0이 되기 때문.
+        countMap.put(0, 1);
+        // 정답
         long answer = 0;
-        for(int j=1; j<=N; j++){
-            answer += map.getOrDefault(arr[j] - K, 0);
-            map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
+
+        for (int i = 1; i <= N; i++) {
+            answer += countMap.getOrDefault(prefixSum[i] - K, 0);
+            countMap.put(prefixSum[i], countMap.getOrDefault(prefixSum[i], 0) + 1);
         }
 
         System.out.println(answer);
     }
+
 }
 
