817032d839b719612baa055cd9ebb6719b148eaf
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-03 18:48:24 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 608 ms, Memory: 71268 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,6 +1,6 @@
-import javax.xml.crypto.dsig.keyinfo.KeyInfo;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -8,32 +8,34 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sa = bf.readLine().split(" ");
+
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
 
-        int[] ori = new int[N + 1];
-        int[] prefix = new int[N + 1];
-
+        int[] arr = new int[N];
         sa = bf.readLine().split(" ");
-
         for (int i = 0; i < N; i++) {
-            ori[i + 1] = Integer.parseInt(sa[i]);
+            arr[i] = Integer.parseInt(sa[i]);
         }
 
-        for (int i = 1; i <= N; i++) {
-            prefix[i] = prefix[i - 1] + ori[i];
+        // 누적합 배열 생성
+        int[] prefix = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
         }
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < M; i++) {
+        for (int t = 0; t < M; t++) {
             sa = bf.readLine().split(" ");
-            int start = Integer.parseInt(sa[0]);
-            int end = Integer.parseInt(sa[1]);
+            int s = Integer.parseInt(sa[0]);
+            int e = Integer.parseInt(sa[1]);
 
-            sb.append(prefix[end] - prefix[start - 1]).append("\n");
+            sb.append(prefix[e] - prefix[s - 1]).append("\n");
         }
 
         System.out.println(sb);
     }
+
 }
+
