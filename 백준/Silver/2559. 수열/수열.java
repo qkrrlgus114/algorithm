a880f204f1214e00ad33bdc2ccd4c1086fcadff6
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-04 20:09:48 +0900

    [Silver III] Title: 수열, Time: 196 ms, Memory: 24720 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,36 +1,31 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
+
         int N = Integer.parseInt(sa[0]);
         int K = Integer.parseInt(sa[1]);
 
-        int[] arr = new int[N+1];
+        int[] arr = new int[N];
         sa = bf.readLine().split(" ");
-        for(int i=1; i<N+1; i++){
-            arr[i] = Integer.parseInt(sa[i-1]);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(sa[i]);
         }
 
-        int[] pSum = new int[N+1];
-        for(int i=1; i<N+1; i++){
-            pSum[i] = pSum[i-1] + arr[i];
+        int[] prefix = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
         }
 
         int answer = Integer.MIN_VALUE;
-        for(int i=K; i<N+1; i++){
-            int value = pSum[i] - pSum[i-K];
-            if(value > answer) answer = value;
+        for (int i = N; i >= K; i--) {
+            answer = Math.max(answer, prefix[i] - prefix[i - K]);
         }
 
         System.out.println(answer);
