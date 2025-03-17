a0b3f545e225b59d86bed2cf9a8860ebee51c290
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-17 11:38:25 +0900

    [Silver III] Title: N과 M (7), Time: 556 ms, Memory: 220060 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,51 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
 
 public class Main {
 
     static int N, M;
-    static List<Integer> pick;
-    static int[] arr;
+    static boolean[] visited = new boolean[10001];
     static StringBuilder sb = new StringBuilder();
+    static int[] answer;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] sa = bf. readLine().split(" ");
+        String[] sa = bf.readLine().split(" ");
+
         N = Integer.parseInt(sa[0]);
         M = Integer.parseInt(sa[1]);
-        pick = new ArrayList<>();
+
+        answer = new int[M];
         arr = new int[N];
 
         sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(sa[i]);
         }
-
         Arrays.sort(arr);
 
-        combination(0);
-        System.out.println(sb);
+        recur(0);
 
+        System.out.println(sb);
     }
 
-    public static void combination(int num){
-        // 종료 조건
-        if(num == M){
-            for(int i : pick){
-                sb.append(i).append(" ");
+    public static void recur(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i=0; i<N; i++){
-            pick.add(arr[i]);
-            combination(num + 1);
-            pick.remove(pick.size() - 1);
+        for (int i = 0; i < N; i++) {
+//            if (visited[arr[i]]) continue;
+//            visited[arr[i]] = true;
+            answer[depth] = arr[i];
+            recur(depth + 1);
+//            visited[arr[i]] = false;
         }
     }
 }
