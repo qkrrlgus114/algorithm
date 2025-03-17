76eaf2500fdc87eb05a80ea21d9fd37b4334be57
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-17 12:51:30 +0900

    [Silver II] Title: N과 M (12), Time: 80 ms, Memory: 12056 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -2,55 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-
 
 public class Main {
-    static int[] arr;
-    static int[] result;
-    static boolean[] visited;
-    static int N;
-    static int M;
+
+    static int N, M;
+    static boolean[] visited = new boolean[10001];
     static StringBuilder sb = new StringBuilder();
+    static int[] answer;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = bf.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
 
+        String[] sa = bf.readLine().split(" ");
+
+        N = Integer.parseInt(sa[0]);
+        M = Integer.parseInt(sa[1]);
+
+        answer = new int[M];
         arr = new int[N];
-        result = new int[M];
-//        visited = new boolean[N];
+        visited = new boolean[N];
 
-        s = bf.readLine().split(" ");
+        sa = bf.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(s[i]);
+            arr[i] = Integer.parseInt(sa[i]);
         }
         Arrays.sort(arr);
-        back( 0,0);
-        System.out.print(sb.toString());
+
+        recur(0, 0);
+
+        System.out.println(sb);
     }
 
-    public static void back(int depth,int count) {
-        if (count == M) {
-            for (Integer i : result) {
-                sb.append(i + " ");
+    public static void recur(int depth, int num) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        // 과거의 값
-        int past = -1;
-
-        for (int i = depth; i < N; i++) {
-            if (past != arr[i]) {
-                past = arr[i];
-                result[count] = arr[i];
-                back(i, count + 1);
-//                visited[i] = false;
-            }
+        int before = -1;
+        for (int i = num; i < N; i++) {
+            if (before == arr[i]) continue;
+            before = arr[i];
+//            visited[i] = true;
+            answer[depth] = arr[i];
+            recur(depth + 1, i);
+//            visited[i] = false;
         }
     }
 }
