8056f13cd6d3812ee0f8fa878d78a28d1feeaca5
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-17 09:19:31 +0900

    [Silver III] Title: N과 M (4), Time: 84 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,42 +2,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
-    static int[] arr;
-    static int[] result;
-    //    static boolean[] visited;
-    static int N;
-    static int M;
+
+    static int N, M;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = bf.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
 
-        arr = new int[N];
-        result = new int[M];
-        for (int i = 1; i <= N; i++) {
-            arr[i - 1] = i;
-        }
-        back(0, 0);
-        System.out.println(sb.toString());
+        String[] sa = bf.readLine().split(" ");
+
+        N = Integer.parseInt(sa[0]);
+        M = Integer.parseInt(sa[1]);
+
+        visited = new boolean[N + 1];
+        arr = new int[M];
+
+        recur(0, 1);
+
+        System.out.println(sb);
     }
 
-    public static void back(int depth, int count) {
-        if (count == M) {
-            for(Integer i : result){
-                sb.append(i + " ");
+    public static void recur(int depth, int num) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = depth; i < N; i++) {
-            result[count] = i + 1;
-            back(i, count + 1);
+        for (int i = num; i <= N; i++) {
+//            if (visited[i]) continue;
+//            visited[i] = true;
+            arr[depth] = i;
+            recur(depth + 1, i);
+//            visited[i] = false;
         }
     }
 }
