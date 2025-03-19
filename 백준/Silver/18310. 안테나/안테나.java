ab493fc361bf5fd2f00efff7de564abb57b0f2c2
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-19 22:07:05 +0900

    [Silver III] Title: 안테나, Time: 344 ms, Memory: 43536 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,32 +1,24 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
 
+        int N = Integer.parseInt(bf.readLine());
+
+        int[] arr = new int[N];
         String[] sa = bf.readLine().split(" ");
-        int[] home = new int[N];
-        for(int i=0; i<N; i++){
-            home[i] = Integer.parseInt(sa[i]);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(sa[i]);
         }
+        Arrays.sort(arr);
 
-        Arrays.sort(home);
-        int mid = N / 2;
+        System.out.println(arr.length % 2 == 0 ? arr[(arr.length / 2) - 1] : arr[arr.length / 2]);
 
-        if(N % 2 == 0) System.out.println(home[mid -1 ]);
-        else System.out.println(home[mid]);
     }
 
 }
-
