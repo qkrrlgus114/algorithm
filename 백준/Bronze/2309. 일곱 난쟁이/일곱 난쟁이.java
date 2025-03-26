2588ec4d3cd5114c48808ae82fd8b37f2b6bff82
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-26 20:57:27 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,37 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<Integer> list = new LinkedList<>();
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int sum = 0;
-        for(int i=0; i<9; i++){
-            list.add(Integer.parseInt(bf.readLine()));
-            sum += list.get(i);
-        }
 
-        outer: for(int i=0; i<list.size() - 1; i++){
-            for(int j=i+1; j<list.size(); j++){
-                if(sum - list.get(i) - list.get(j) == 100){
-                    list.remove(j);
-                    list.remove(i);
+        int[] nan = new int[9];
+        int sum = 0; // 난쟁이 키의 합
+        for (int i = 0; i < 9; i++) {
+            nan[i] = Integer.parseInt(bf.readLine());
+            sum += nan[i];
+        }
+        Arrays.sort(nan);
+
+        StringBuilder sb = new StringBuilder();
+
+        // 난쟁이 총 합에서 2명을 뺀 것이 100이 되면 정답
+        outer:
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - nan[i] - nan[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k != i && k != j) sb.append(nan[k]).append("\n");
+                    }
                     break outer;
                 }
             }
         }
 
-        Collections.sort(list);
-
-        StringBuilder sb = new StringBuilder();
-        for(int num : list){
-            sb.append(num).append("\n");
-        }
         System.out.println(sb);
-
 
     }
 }
