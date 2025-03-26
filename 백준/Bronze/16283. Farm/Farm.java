8c11e4dc3f8d89977d0c2968f15c76e72fe6a45b
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-26 20:34:35 +0900

    [Bronze II] Title: Farm, Time: 84 ms, Memory: 12188 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,33 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
+        int a = Integer.parseInt(sa[0]); // 양 사료
+        int b = Integer.parseInt(sa[1]); // 염소 사료
+        int n = Integer.parseInt(sa[2]); // 양 + 염소 수
+        int w = Integer.parseInt(sa[3]); // 전체 소비 사료
 
-        int a = Integer.parseInt(sa[0]); // 양 한마리 사료
-        int b = Integer.parseInt(sa[1]); // 염소 한마리 사료
-        int n = Integer.parseInt(sa[2]); // 양+염소
-        int w = Integer.parseInt(sa[3]); // 소비한 사료
+        StringBuilder sb = new StringBuilder();
 
-        int sheep = 0;
-
-        for(int i=1; i<=n-1; i++){
-            if(i * a + (n-i) * b == w){
-                if(sheep == 0){
-                    sheep = i;
-                }else{
-                    System.out.println(-1);
-                    return;
+        // 양
+        outer:
+        for (int i = 1; i <= n; i++) {
+            // 염소
+            for (int j = 1; j <= n; j++) {
+                if (i + j > n) break;
+                // 양사료 + 염소사료 / 양 + 염소
+                if ((i * a) + (j * b) == w && i + j == n) {
+                    if (sb.length() != 0) {
+                        sb.setLength(0);
+                        break outer;
+                    }
+                    sb.append(i).append(" ").append(j);
                 }
             }
         }
 
-        if(sheep == 0) System.out.println(-1);
-        else System.out.println(sheep + " " + (n - sheep));
+        System.out.println(sb.length() == 0 ? -1 : sb);
     }
 }
