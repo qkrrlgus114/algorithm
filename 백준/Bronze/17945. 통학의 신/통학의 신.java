5b04d4ffd3ec38623fd7f8923888a3c8b51aaa33
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-26 20:23:50 +0900

    [Bronze III] Title: 통학의 신, Time: 96 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,20 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
 
+        String[] sa = bf.readLine().split(" ");
         int A = Integer.parseInt(sa[0]);
         int B = Integer.parseInt(sa[1]);
-        
-        for(int i=-1000; i<=1000; i++){
-            if(i * i + 2 * A * i + B == 0) System.out.print(i + " ");
+
+        // 정답 리스트
+        List<Integer> answers = new ArrayList<>();
+
+        for (int i = -1000; i <= 1000; i++) {
+            for (int j = i; j <= 1000; j++) {
+                if (i * j == B && i + j == A * 2) {
+                    // 중근이면
+                    if (i == j) {
+                        answers.add(-1 * i);
+                    } else {
+                        answers.add(-1 * i);
+                        answers.add(-1 * j);
+                    }
+                }
+            }
         }
 
+        Collections.sort(answers);
+        StringBuilder sb = new StringBuilder();
+        for (Integer answer : answers) {
+            sb.append(answer).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
