326c399e9fa03bddb6d9ce04308b68f217b3a9c7
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-27 21:10:46 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,36 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
+    /**
+     *
+     */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int[] mushroom = new int[10];
-        for(int i=0; i<10; i++){
-            String s = bf.readLine();
-            mushroom[i] = Integer.parseInt(s);
-        }
 
-        int max = Integer.MIN_VALUE;
-        int diffValue = Integer.MAX_VALUE;
+        int answer = 0;
+        int value = Integer.MAX_VALUE; // 절댓값
+        int sum = 0;
 
-        for(int i=0; i<10; i++){
-            int sum = 0;
-            for(int j=10-i-1; j>=0; j--){
-                sum += mushroom[j];
-            }
-            int value = 100 - sum;
-
-            if(diffValue > Math.abs(value)){
-                max = sum;
-                diffValue = Math.abs(value);
-            }else if(diffValue == Math.abs(value)){
-                max = Math.max(max, sum);
+        for (int i = 0; i < 10; i++) {
+            sum += Integer.parseInt(bf.readLine());
+            if (Math.abs(100 - sum) <= value) {
+                answer = sum;
+                value = Math.abs(100 - sum);
             }
         }
 
-        System.out.println(max);
+        System.out.println(answer);
     }
 }
