c69244e89727da4e5ad2de17cfcb12f5276f269c
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-27 21:25:17 +0900

    [Bronze II] Title: 방 배정하기, Time: 108 ms, Memory: 13124 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -1,30 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
+    /**
+     * 모든 인원을 배정해라.
+     */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = bf.readLine().split(" ");
 
-        int A = Integer.parseInt(sa[0]);
-        int B = Integer.parseInt(sa[1]);
-        int C = Integer.parseInt(sa[2]);
+        String[] sa = bf.readLine().split(" ");
+        int room1 = Integer.parseInt(sa[0]);
+        int room2 = Integer.parseInt(sa[1]);
+        int room3 = Integer.parseInt(sa[2]);
         int N = Integer.parseInt(sa[3]);
 
-        boolean check = false;
-        for(int i=0; i<=N/A; i++){
-            for(int j=0; j<=N/B;j ++){
-                for(int k=0; k<=N/C; k++){
-                    if(i*A + j*B + k*C == N) {
-                        check = true;
-                        break;
+        // 방 하나로 처리되면
+        for (int i = 0; i <= 300; i++) {
+            for (int j = 0; j <= 300; j++) {
+                for (int k = 0; k <= 300; k++) {
+                    if ((room1 * i) + (room2 * j) + (room3 * k) == N) {
+                        System.out.println(1);
+                        System.exit(0);
                     }
                 }
             }
         }
-        System.out.println(check ? 1 : 0);
+
+        System.out.println(0);
     }
 }
