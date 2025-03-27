01995a8621110ea3aee038676f609995f74550ef
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-27 20:26:29 +0900

    [Bronze III] Title: 완전 제곱수, Time: 72 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
+    /**
+     * A^2 = B^2 + N
+     */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
         int answer = 0;
-
-        String s = bf.readLine();
-        int N = Integer.parseInt(s);
-
-        for(int i=1; i<=500; i++){
-            for(int j=1; j<=500; j++){
-                if(i == j) continue;
-                if(i*i == (j*j) + N) answer++;
+        for (int i = 1; i <= 500; i++) {
+            for (int j = 1; j <= 500; j++) {
+                if (i * i == (j * j) + N) answer++;
             }
         }
 
