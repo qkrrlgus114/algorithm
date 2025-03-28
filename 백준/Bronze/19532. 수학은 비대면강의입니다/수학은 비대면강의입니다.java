088183fa48a96384d1cb879747ca0d9ed869dc44
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-28 21:33:50 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 88 ms, Memory: 11940 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,12 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
+    /**
+     *
+     */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         int a = Integer.parseInt(sa[0]);
         int b = Integer.parseInt(sa[1]);
@@ -15,13 +19,16 @@ public class Main {
         int e = Integer.parseInt(sa[4]);
         int f = Integer.parseInt(sa[5]);
 
-        for(int i=-999; i<=999; i++){
-            for(int j=-999; j<=999; j++){
-                if(a*i + b*j == c && d*i + e*j == f){
+        outer:
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if ((a * i) + (b * j) == c && (d * i) + (e * j) == f) {
                     System.out.println(i + " " + j);
-                    return;
+                    break outer;
                 }
             }
         }
+
+
     }
 }
