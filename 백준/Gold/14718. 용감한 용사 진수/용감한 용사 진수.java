9ad9c63e9dee22a9882c7854a8e72f390a0dc487
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-03-30 15:59:37 +0900

    [Gold IV] Title: 용감한 용사 진수, Time: 604 ms, Memory: 13580 KB -BaekjoonHub

--- 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.java
+++ 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.java
@@ -1,46 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    
+    /**
+     * 0부터 300만까지 모든 스탯으로 몬스터를 잡으면 3억으로 시초가 발생한다.
+     * 
+     * 몬스터의 스탯을 기준으로 돌면서 탐색을 해야 함.
+     * */
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
-
-        int N, K = 0;
-        N = Integer.parseInt(sa[0]);
-        K = Integer.parseInt(sa[1]);
-
-        int[][] monster = new int[N][3];
-        for(int i=0; i<N; i++){
-            sa = bf.readLine().split(" ");
-            monster[i][0] = Integer.parseInt(sa[0]);
-            monster[i][1] = Integer.parseInt(sa[1]);
-            monster[i][2] = Integer.parseInt(sa[2]);
-        }
+        int N = Integer.parseInt(sa[0]);
+        int K = Integer.parseInt(sa[1]);
 
         int answer = Integer.MAX_VALUE;
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                for(int k=0; k<N; k++){
-                    int count = 0;
-                    for(int n=0; n<N; n++){
-                        if(monster[i][0] >= monster[n][0] && monster[j][1] >= monster[n][1] && monster[k][2] >= monster[n][2]){
-                            count++;
+        List<Node> nodeList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            sa = bf.readLine().split(" ");
+            nodeList.add(new Node(Integer.parseInt(sa[0]), Integer.parseInt(sa[1]), Integer.parseInt(sa[2])));
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    int hit = 0; // 잡은 몬스터
+
+                    for (int l = 0; l < N; l++) {
+                        Node node = nodeList.get(l); // 몬스터
+                        if (nodeList.get(i).str >= node.str &&
+                                nodeList.get(j).dex >= node.dex &&
+                                nodeList.get(k).inte >= node.inte) {
+                            hit++;
                         }
                     }
-                    if(count >= K){
-                        answer = Math.min(answer, monster[i][0] + monster[j][1] + monster[k][2]);
+
+                    if (hit >= K) {
+                        answer = Math.min(answer, nodeList.get(i).str + nodeList.get(j).dex + nodeList.get(k).inte);
                     }
                 }
             }
         }
-        System.out.println(answer);
 
+        System.out.println(answer);
     }
 }
 
+class Node {
+    int str;
+    int dex;
+    int inte;
+
+    public Node(int str, int dex, int inte) {
+        this.str = str;
+        this.dex = dex;
+        this.inte = inte;
+    }
+}
