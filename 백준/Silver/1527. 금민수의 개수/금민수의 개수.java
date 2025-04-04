f9a016afc7282ad0b66f5db16c49e332f1f1c3bb
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-04 23:20:29 +0900

    [Silver I] Title: 금민수의 개수, Time: 80 ms, Memory: 12192 KB -BaekjoonHub

---
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static List<Integer> list = new ArrayList<>();
+    static int[] arr = new int[]{4, 7};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] sa = bf.readLine().split(" ");
+
+
+        String A = sa[0];
+        String B = sa[1];
+        int a = Integer.parseInt(A);
+        int b = Integer.parseInt(B);
+        if (a == 1000000000) {
+            a--;
+            A = String.valueOf(a);
+        }
+        if (b == 1000000000) {
+            b--;
+            B = String.valueOf(b);
+        }
+        int answer = 0;
+
+        if (A.length() == B.length()) {
+            dfs(new StringBuilder(), A.length());
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) >= a && list.get(i) <= b) answer++;
+            }
+        } else {
+            for (int i = A.length(); i <= B.length(); i++) {
+                dfs(new StringBuilder(), i);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) >= a && list.get(i) <= b) answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    // 두 수가 길이가 같은 경우 해당 길이의 경우의 수만 구한다.
+    public static void dfs(StringBuilder sb, int length) {
+        if (sb.length() == length) {
+            list.add(Integer.parseInt(sb.toString()));
+            return;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            dfs(sb.append(arr[i]), length);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+    }
+
+}
