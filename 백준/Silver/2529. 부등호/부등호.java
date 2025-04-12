e293e9f0b29fbe340e167954269cad9517eee61c
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-13 01:03:34 +0900

    [Silver I] Title: 부등호, Time: 116 ms, Memory: 16648 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,74 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    static String[] list;
-    static int N;
-    static Long max = Long.MIN_VALUE;
-    static Long min = Long.MAX_VALUE;
-    static String str_min = "";
-    static String str_max = "";
-    static int[] numbers;
+    static int K;
+    static String[] arr; // 부등호
+    static List<String> list = new ArrayList<>(); // 정답을 저장해놓을 리스트
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String input = bf.readLine();
-        N = Integer.parseInt(input);
-        list = new String[N];
-        list = bf.readLine().split(" ");
-        boolean[] visited = new boolean[10];
-        numbers = new int[N + 1];
 
-        for(int i=0; i<10; i++){
-            numbers[0] = i;
-            visited[i] = true;
-            dfs(0, visited);
-            visited[i] = false;
+        K = Integer.parseInt(bf.readLine());
+
+        arr = new String[K];
+        visited = new boolean[10];
+        String[] sa = bf.readLine().split(" ");
+        for (int i = 0; i < K; i++) {
+            arr[i] = sa[i];
         }
-        System.out.println(str_max);
-        System.out.println(str_min);
+
+        combination(0, new StringBuilder());
+
+        System.out.println(list.get(list.size() - 1));
+        System.out.println(list.get(0));
+
+
     }
 
-    public static void dfs(int depth, boolean[] visited){
+    public static void combination(int choice, StringBuilder sb) {
         // 종료 조건
-        if(depth == N){
-            StringBuilder temp = new StringBuilder();
-            for(int i=0; i<numbers.length; i++){
-                temp.append(numbers[i]);
-            }
-
-            Long temp_long = Long.valueOf(temp.toString());
-            if(temp_long < min){
-                min = temp_long;
-                str_min = temp.toString();
-            }else if(temp_long > max){
-                max = temp_long;
-                str_max = temp.toString();
-            }
+        if (choice == K + 1) {
+            list.add(sb.toString());
             return;
         }
 
-        for(int i=0; i<10; i++){
-            if(visited[i]) continue;
-            if(list[depth].equals("<")){
-                if(numbers[depth] < i){
-                    visited[i] = true;
-                    numbers[depth + 1] = i;
-                    dfs(depth + 1, visited);
-                    visited[i] = false;
-                }
-            }else if(list[depth].equals(">")){
-                if(numbers[depth] > i){
-                    visited[i] = true;
-                    numbers[depth + 1] = i;
-                    dfs(depth + 1, visited);
-                    visited[i] = false;
-                }
+
+        // 재귀
+        for (int i = 0; i <= 9; i++) {
+            if (visited[i]) continue;
+            if (choice > 0) {
+                if (arr[choice - 1].equals(">") && sb.charAt(choice - 1) < (char) (i + '0')) continue;
+                if (arr[choice - 1].equals("<") && sb.charAt(choice - 1) > (char) (i + '0')) continue;
             }
+            visited[i] = true;
+            sb.append(i);
+            combination(choice + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+            visited[i] = false;
         }
     }
 
 }
+
