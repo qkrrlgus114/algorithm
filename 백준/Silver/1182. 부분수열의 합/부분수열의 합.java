d077ec774ea087cdc40f8aaf6ac3aa69c47df551
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-13 18:16:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 72 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    static int N, S;
-    static int result = 0;
+    static int N, S, answer;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -16,26 +13,29 @@ public class Main {
         String[] sa = bf.readLine().split(" ");
         N = Integer.parseInt(sa[0]);
         S = Integer.parseInt(sa[1]);
-
         arr = new int[N];
         sa = bf.readLine().split(" ");
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(sa[i]);
         }
 
-        combination(0, 0);
+        permutation(0, 0, false);
 
-        System.out.println(result);
+        System.out.println(answer);
 
     }
 
-    public static void combination(int index, int sum){
-        if(index == N) return;
-        if(sum + arr[index] == S) result++;
+    public static void permutation(int depth, int sum, boolean flag) {
+        if (sum == S && flag) {
+            answer++;
+        }
+        if (depth >= N) return;
 
-        // 현재 안 뽑는 경우
-        combination(index + 1, sum);
-        // 현재 뽑는 경우
-        combination(index + 1, sum + arr[index]);
+        // 고르는 경우
+        permutation(depth + 1, sum + arr[depth], true);
+        // 안고르는 경우
+        permutation(depth + 1, sum, false);
     }
+
 }
+
