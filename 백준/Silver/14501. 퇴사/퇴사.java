31ace82a4a702e397b99217d2884e99db749c471
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-13 17:46:29 +0900

    [Silver III] Title: 퇴사, Time: 72 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,49 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
 
 public class Main {
 
-    static int N;
-    static int[][] arr;
-    static int[] memo;
-    static int max = Integer.MIN_VALUE;
+    static int N, answer;
+    static int[] T;
+    static int[] P;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String n = bf.readLine();
-        N = Integer.parseInt(n);
-        arr = new int[N][2];
-        memo = new int[N];
-        Arrays.fill(memo, -1);
-        for(int i=0; i<N; i++){
+
+        N = Integer.parseInt(bf.readLine());
+        T = new int[N];
+        P = new int[N];
+
+        for (int i = 0; i < N; i++) {
             String[] sa = bf.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(sa[0]);
-            arr[i][1] = Integer.parseInt(sa[1]);
+            T[i] = Integer.parseInt(sa[0]);
+            P[i] = Integer.parseInt(sa[1]);
         }
 
-        int dp = dp(0);
-        System.out.println(dp);
+        combination(0, 0);
 
+        System.out.println(answer);
     }
 
-    public static int dp(int i){
-        if(i >= N){
-            return 0;
+    public static void combination(int depth, int sum) {
+        // 종료 조건
+        if (depth >= N) {
+            answer = Math.max(answer, sum);
+            return;
         }
-        if(memo[i] != -1){
-            return memo[i];
-        }
-        int take = 0;
-        if(i + arr[i][0] <= N){
-            take = dp(i + arr[i][0]) + arr[i][1];
-        }
-        int non_take = dp(i + 1);
 
-        return Math.max(take, non_take);
+        // 고르는 경우
+        if (depth + T[depth] <= N) {
+            combination(depth + T[depth], sum + P[depth]);
+        }
+        // 안고르는 경우
+        combination(depth + 1, sum);
     }
-
 
 }
+
