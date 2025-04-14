286c786e7728033b5e2b916fb99881e34c0c3f7f
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-14 22:38:57 +0900

    [Silver II] Title: 차이를 최대로, Time: 88 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,59 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class Main {
-    static int N;
+
+    static int N, answer;
     static int[] arr;
-    static boolean[] visited;
     static int[] choice;
-    static int result =Integer.MIN_VALUE;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bf.readLine();
-        N = Integer.parseInt(s);
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
+        choice = new int[N];
+        visited = new boolean[N];
 
         String[] sa = bf.readLine().split(" ");
-        arr = new int[N];
-        visited = new boolean[N];
-        choice = new int[N];
-
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(sa[i]);
         }
 
-        dfs(0);
-        System.out.println(result);
+        combination(0);
 
+        System.out.println(answer);
 
     }
 
-    public static void dfs(int depth){
-        if(depth == N){
+    public static void combination(int depth) {
+        if (depth == N) {
             int sum = 0;
-            for(int i=0; i<N-1; i++){
-                sum += Math.abs(choice[i] - choice[i+1]);
+            for (int i = 1; i < N; i++) {
+                sum += Math.abs(choice[i - 1] - choice[i]);
             }
-            result = Math.max(result, sum);
+            answer = Math.max(sum, answer);
             return;
         }
 
-        for(int i=0; i<N; i++){
-            if(visited[i]) continue;
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
             visited[i] = true;
             choice[depth] = arr[i];
-            dfs(depth + 1);
+            combination(depth + 1);
             visited[i] = false;
         }
+
+
     }
 
+
 }
-
-
 
