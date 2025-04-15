e233379356d17cf2de9f86b2498d6e0c70350584
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-16 00:03:48 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 13192 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,43 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    static int[][] dp;
-    static int[][] arr;
-    static int min = Integer.MAX_VALUE;
+
+    static int N;
+    static int answer = Integer.MAX_VALUE;
+    static List<int[]> arr = new ArrayList<>();
+    static int[][] memo;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String n = bf.readLine();
-        int N = Integer.parseInt(n);
-        dp = new int[N+1][3];
-        arr = new int[N+1][3];
 
-        for(int i=1; i<=N; i++){
+        N = Integer.parseInt(bf.readLine());
+        memo = new int[N][3];
+
+        for (int i = 0; i < N; i++) {
             String[] sa = bf.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(sa[0]);
-            arr[i][1] = Integer.parseInt(sa[1]);
-            arr[i][2] = Integer.parseInt(sa[2]);
+            arr.add(new int[]{Integer.parseInt(sa[0]), Integer.parseInt(sa[1]), Integer.parseInt(sa[2])});
+            Arrays.fill(memo[i], -1);
         }
 
-        int result = Integer.MAX_VALUE;
+        int red = dp(0, 0);
+        int green = dp(0, 1);
+        int blue = dp(0, 2);
 
-        for(int i=1; i<=N; i++){
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
+        System.out.println(Math.min(Math.min(red, green), blue));
+    }
+
+    public static int dp(int cur, int prev) {
+        if (cur == N) {
+            return 0;
+        }
+        if (memo[cur][prev] != -1) {
+            return memo[cur][prev];
         }
 
-        for(int i=0; i<3; i++){
-            if(dp[N][i] < result) result = dp[N][i];
+        int value = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            if (i == prev) continue;
+            value = Math.min(value, dp(cur + 1, i) + arr.get(cur)[i]);
         }
 
-        System.out.println(result);
-
-
-
+        return memo[cur][prev] = value;
     }
 
 }
