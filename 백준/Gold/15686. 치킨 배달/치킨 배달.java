3a6a13f37e91935c01491738869aed8683e69cc4
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-16 23:02:09 +0900

    [Gold V] Title: 치킨 배달, Time: 124 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,93 +1,117 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int[][] chicken_house;
-    static int[][] house;
-    static int[][] map;
-    static boolean[] seleted;
-    static int min = Integer.MAX_VALUE;
+    static int N, M;
+    // 집의 좌표
+    static List<Home> homes = new ArrayList<>();
+    // 치킨집의 좌표
+    static List<Chick> chicks = new ArrayList<>();
+    // 치킨집을 선택했는지
+    static boolean[] visited;
+    // 해당 치킨집과 집의 거리
+    static int[][] distance;
+
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sa = bf.readLine().split(" ");
         N = Integer.parseInt(sa[0]);
         M = Integer.parseInt(sa[1]);
 
-        map = new int[N][N];
-
-        int chicken_count = 0;
-        int house_count = 0;
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             sa = bf.readLine().split(" ");
-            for(int j=0; j<N; j++){
-                map[i][j] = Integer.parseInt(sa[j]);
-                if(map[i][j] == 1) house_count++;
-                else if(map[i][j] == 2) chicken_count++;
-            }
-        }
-
-        seleted = new boolean[chicken_count];
-        chicken_house = new int[chicken_count][2];
-        house = new int[house_count][2];
-        int house_index = 0;
-        int chicken_index = 0;
-
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(map[i][j] == 1){
-                    house[house_index][0] = i;
-                    house[house_index++][1] = j;
-                }else if(map[i][j] == 2){
-                    chicken_house[chicken_index][0] = i;
-                    chicken_house[chicken_index++][1] = j;
+            for (int j = 0; j < N; j++) {
+                int value = Integer.parseInt(sa[j]);
+                if (value == 1) {
+                    homes.add(new Home(i, j));
+                } else if (value == 2) {
+                    chicks.add(new Chick(i, j));
                 }
             }
         }
 
-        dfs(0, 0);
-        System.out.println(min);
+
+        distance = new int[chicks.size()][homes.size()];
+        for (int i = 0; i < chicks.size(); i++) {
+            Arrays.fill(distance[i], Integer.MAX_VALUE);
+        }
+        visited = new boolean[chicks.size()];
+
+        recur(0, 0);
+
+        System.out.println(answer);
+
     }
 
-    public static void dfs(int depth, int index){
-        // 종료 조건
-        if(depth == M){
-            int sum = 0;
-            for(int i=0; i<house.length; i++){
-                int distance = Integer.MAX_VALUE;
-                int y1 = house[i][0];
-                int x1 = house[i][1];
-                for(int j=0; j<chicken_house.length; j++){
-                    int temp_dis = 0;
-                    // 선택한 치킨 집만 계산
-                    if(seleted[j]){
-                        int y2 = chicken_house[j][0];
-                        int x2 = chicken_house[j][1];
-                        temp_dis = Math.abs(y1 - y2) + Math.abs(x1 - x2);
-                        if(distance > temp_dis) distance = temp_dis;
-                    }
-                }
-                sum += distance;
-                if(min < sum) return;
-            }
-            if(min > sum) min = sum;
-
+    public static void recur(int choice, int index) {
+        // 최소의 치킨거리 파악
+        if (choice > 0) {
+            calculation();
+        }
+        if (choice >= M) {
             return;
         }
 
-        for(int i=index; i<seleted.length; i++){
-            if(!seleted[i]){
-                seleted[i] = true;
-                dfs(depth + 1, i + 1);
-                seleted[i] = false;
+
+        for (int i = index; i < chicks.size(); i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            // 치킨 거리 미리 계산해서 넣어놓기
+            if (distance[i][0] == Integer.MAX_VALUE) {
+                for (int j = 0; j < homes.size(); j++) {
+                    distance[i][j] = Math.abs(chicks.get(i).y - homes.get(j).y) + Math.abs(chicks.get(i).x - homes.get(j).x);
+                }
+            }
+            recur(choice + 1, i + 1);
+            visited[i] = false;
+        }
+    }
+
+    // 치킨 거리 파악
+    public static void calculation() {
+        int sum = 0;
+        for (int i = 0; i < homes.size(); i++) {
+            int value = Integer.MAX_VALUE;
+            for (int j = 0; j < chicks.size(); j++) {
+                if (!visited[j]) continue;
+                value = Math.min(value, distance[j][i]);
+            }
+            if (value != Integer.MAX_VALUE) {
+                sum += value;
             }
         }
+
+        if (sum != 0) {
+            answer = Math.min(answer, sum);
+        }
+    }
+}
+
+class Home {
+    int y;
+    int x;
+
+    public Home(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
+
+class Chick {
+    int y;
+    int x;
+
+    public Chick(int y, int x) {
+        this.y = y;
+        this.x = x;
     }
 }
