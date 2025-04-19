4c4071298f0dc0d9014f633c50e6c8210e364b08
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-04-19 20:27:29 +0900

    [Gold V] Title: 퇴사 2, Time: 916 ms, Memory: 327300 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,41 +1,49 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    static int N, answer;
+    static List<int[]> list = new ArrayList<>();
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bf.readLine());
+        N = Integer.parseInt(bf.readLine());
+        dp = new int[N + 1];
 
-        int[] T = new int[N + 1];
-        int[] P = new int[N + 1];
-        int[] memo = new int[N + 1];
-        int result = 0;
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             String[] sa = bf.readLine().split(" ");
-            T[i] = Integer.parseInt(sa[0]);
-            P[i] = Integer.parseInt(sa[1]);
+            list.add(new int[]{Integer.parseInt(sa[0]), Integer.parseInt(sa[1])});
         }
 
-        for (int i = 1; i <= N; i++) {
-            memo[i] = Math.max(memo[i], memo[i-1]);
-            if(i + T[i] - 1 > N) continue;
-            memo[i + T[i] - 1] = Math.max(memo[i + T[i] - 1], memo[i - 1] + P[i]);
+        for (int i = 0; i < N; i++) {
+            int[] info = list.get(i);
+            int day = info[0];
+            int pay = info[1];
+
+            // 상담을 하는 경우
+            if (i + day <= N) {
+                dp[i + day] = Math.max(dp[i + day], dp[i] + pay);
+            }
+
+            // 상담을 하지 않는 경우
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
-        
-        for(int i=1; i<=N; i++){
-            result = Math.max(result, memo[i]);
+
+        int answer = 0;
+        for (int i = 0; i <= N; i++) {
+            answer = Math.max(answer, dp[i]);
         }
 
-        System.out.println(result);
 
-
+        System.out.println(answer);
     }
+
+
 }
-
-
