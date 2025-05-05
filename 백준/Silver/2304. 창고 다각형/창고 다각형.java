85455d441df9ca1323741a571165acb9210aac5d
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-05-05 15:56:01 +0900

    [Silver II] Title: 창고 다각형, Time: 80 ms, Memory: 12804 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
@@ -11,68 +9,53 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bf.readLine());
+        int maxHeight = 0;
+        int leftMaxHeightIdx = 0;
+        int rightMaxHeightIdx = 0;
 
-        List<Block> list = new ArrayList<>();
-
+        int[] arr = new int[1001];
         for (int i = 0; i < N; i++) {
             String[] sa = bf.readLine().split(" ");
-            list.add(new Block(Integer.parseInt(sa[0]), Integer.parseInt(sa[1])));
-        }
+            int L = Integer.parseInt(sa[0]);
+            int H = Integer.parseInt(sa[1]);
 
-        Collections.sort(list, (o1, o2) -> {
-            if (o1.height == o2.height) {
-                return o1.index - o2.index;
-            }
+            arr[L] = H;
 
-            return o1.index - o2.index;
-        });
-
-        int answer = 0;
-
-        int maxHeight = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < N; i++) {
-            if (maxHeight < list.get(i).height) {
-                maxHeight = list.get(i).height;
-                maxIndex = i;
+            // 가장 높은 높이와 그 높이의 왼쪽, 오른쪽 인덱스를 갱신해 나간다.
+            if (maxHeight < H) {
+                maxHeight = H;
+                leftMaxHeightIdx = L;
+                rightMaxHeightIdx = L;
+            } else if (maxHeight == H) {
+                leftMaxHeightIdx = Math.min(leftMaxHeightIdx, L);
+                rightMaxHeightIdx = Math.max(rightMaxHeightIdx, L);
             }
         }
 
-        // 왼쪽 계산
-        int leftHeight = list.get(0).height;
-        int leftIndex = 0;
-        for (int i = 0; i <= maxIndex; i++) {
-            if (list.get(i).height >= leftHeight) {
-                answer += (list.get(i).index - list.get(leftIndex).index) * leftHeight;
-                leftHeight = list.get(i).height;
-                leftIndex = i;
-            }
+        // 왼쪽 누적합, 오른쪽 누적합
+        int[] leftPrefix = new int[1001];
+        int[] rightPrefix = new int[1002];
+
+        for (int i = 1; i <= 1000; i++) {
+            leftPrefix[i] = Math.max(leftPrefix[i - 1], arr[i]);
         }
-        
-        // 오른쪽 계산
-        int rightHeight = list.get(N - 1).height;
-        int rightIndex = N - 1;
-        for (int i = N - 1; i >= maxIndex; i--) {
-            if (list.get(i).height >= rightHeight) {
-                answer += (list.get(rightIndex).index - list.get(i).index) * rightHeight;
-                rightHeight = list.get(i).height;
-                rightIndex = i;
-            }
+        for (int i = 1000; i >= 2; i--) {
+            rightPrefix[i] = Math.max(rightPrefix[i + 1], arr[i]);
         }
 
-        System.out.println(answer + maxHeight);
+        long answer = 0;
+        for (int i = 0; i <= 1000; i++) {
+            if (leftPrefix[i] == maxHeight) break;
+            answer += leftPrefix[i];
+        }
+        for (int i = 1000; i >= 0; i--) {
+            if (rightPrefix[i] == maxHeight) break;
+            answer += rightPrefix[i];
+        }
+        for (int i = leftMaxHeightIdx; i <= rightMaxHeightIdx; i++) {
+            answer += maxHeight;
+        }
 
-    }
-
-}
-
-class Block {
-    int index;
-    int height;
-
-    public Block(int index, int height) {
-        this.index = index;
-        this.height = height;
+        System.out.println(answer);
     }
 }
-
