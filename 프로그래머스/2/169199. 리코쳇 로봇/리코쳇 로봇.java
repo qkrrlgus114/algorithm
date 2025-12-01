7445dac8433ee15a1852972862edb650f23374f5
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-12-01 23:29:35 +0900

    [level 2] Title: 리코쳇 로봇, Time: 8.14 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,100 +1,115 @@
 import java.util.*;
 
-// 끝에 도달하면 멈춘다.
-// D에 부딪히면 멈춘다.
-// 멈춘 지점이 G면 최소 이동 거리
-
 class Solution {
     
-    static char[][] map;
-    static boolean[][] visited;
-    static int N, M;
-    // 상 하 좌 우
+    static int y;
+    static int x;
+    // [상하좌우][y][x]
+    static boolean[][][] visited;
+    static char[][] pan;
+    // 정답
+    static int answer = Integer.MAX_VALUE;
+    // 상하좌우
     static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0 ,0};
-    static int result = -1;
+    static int[] dy = {1, -1, 0, 0};
     
     public int solution(String[] board) {
-        N = board.length;
-        M = board[0].length();
-        map = new char[N][M];
-        visited = new boolean[N][M];
-        for(int i=0; i<N; i++){
-            map[i] = board[i].toCharArray();
+        pan = new char[board.length][board[0].length()];
+        for(int i=0; i<pan.length; i++){
+            pan[i] = board[i].toCharArray();
         }
-        
-        int startY = 0;
-        int startX = 0;
-        
-        outer: for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-               if(map[i][j] == 'R'){
-                   startY = i;
-                   startX = j;
-                   break outer;
-               } 
+        y = pan.length;
+        x = pan[0].length;
+        visited = new boolean[4][y][x];
+
+        outer: for(int i=0; i<y; i++){
+            for(int j=0; j<x; j++){
+                if(pan[i][j] == 'R'){
+                    bfs(i, j);
+                    break outer;
+                }
             }
         }
-        bfs(startY, startX);
         
-
-        return result;
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
     
-    private static void bfs(int y, int x){
+    public static void bfs(int cy, int cx){
         Queue<int[]> q = new LinkedList<>();
-        visited[y][x] = true;
-        q.add(new int[]{y, x, 0});
+        q.add(new int[]{cy, cx, 0});
         
         while(!q.isEmpty()){
             int[] temp = q.poll();
-            y = temp[0];
-            x = temp[1];
-            int step = temp[2];
+            int ty = temp[0];
+            int tx = temp[1];
+            int cnt = temp[2];
             for(int i=0; i<4; i++){
-                temp = moveRobot(y, x, i);
-                if(visited[temp[0]][temp[1]]) continue;
-                if(map[temp[0]][temp[1]] == 'G'){
-                    result = step + 1;
-                    return;
+                int ndy = dy[i] + ty;
+                int ndx = dx[i] + tx;
+                if(ndy < 0 || ndy >= y || ndx < 0 || ndx >= x || pan[ndy][ndx] == 'D'){
+                    continue;
                 }
-                visited[temp[0]][temp[1]] = true;
-                q.add(new int[]{temp[0], temp[1], step + 1});
+                if(visited[i][ndy][ndx]) continue;
+                
+                visited[i][ndy][ndx] = true;
+                
+                int[] result = move(ndy, ndx, i);
+                
+                if(pan[result[0]][result[1]] == 'G'){
+                    answer = Math.min(answer, cnt + 1);
+                    continue;
+                }
+                
+                q.add(new int[]{result[0], result[1], cnt + 1});
             }
         }
     }
     
-    private static int[] moveRobot(int y, int x, int dir){
-        // 상 하 좌 우
+    public static int[] move(int ndy, int ndx, int dir){
+        int[] result = new int[2];
+        // 상
         if(dir == 0){
             while(true){
-                y--;
-                if(y < 0 || map[y][x] == 'D'){
-                    return new int[]{y+1, x};
+                ndy++;
+                if(ndy >= y || pan[ndy][ndx] == 'D'){
+                    result[0] = ndy - 1;
+                    result[1] = ndx;
+                    return result;
                 }
             }
-        }else if(dir == 1){
+        }
+        // 하
+        else if(dir == 1){
             while(true){
-                y++;
-                if(y >= N || map[y][x] == 'D'){
-                    return new int[]{y-1, x};
+                ndy--;
+                if(ndy < 0 || pan[ndy][ndx] == 'D'){
+                    result[0] = ndy + 1;
+                    result[1] = ndx;
+                    return result;
                 }
             }
-        }else if(dir == 2){
+        }
+        // 좌
+        else if(dir == 2){
             while(true){
-                x--;
-                if(x < 0 || map[y][x] == 'D'){
-                    return new int[]{y, x+1};
+                ndx--;
+                if(ndx < 0 || pan[ndy][ndx] == 'D'){
+                    result[0] = ndy;
+                    result[1] = ndx + 1;
+                    return result;
                 }
             }
-        }else{
+        }        
+        // 우
+        else {
             while(true){
-                x++;
-                if(x >= M || map[y][x] == 'D'){
-                    return new int[]{y, x-1};
+                ndx++;
+                if(ndx >= x || pan[ndy][ndx] == 'D'){
+                    result[0] = ndy;
+                    result[1] = ndx - 1;
+                    return result;
                 }
             }
         }
     }
-    
 }
