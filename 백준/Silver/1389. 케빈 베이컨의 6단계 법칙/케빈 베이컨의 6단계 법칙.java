f587ef029cd60788b2f980e73ec899493dc53f1e
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-12-13 15:42:54 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 88 ms, Memory: 12924 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,75 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static int result = Integer.MAX_VALUE;
-	static int humanIdx = Integer.MAX_VALUE;
-	static List<Integer>[] graph;
+    static int N;
+    static int M;
+    static List<Integer>[] list;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String[] sa = bf.readLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.parseInt(sa[0]);
-		M = Integer.parseInt(sa[1]);
+        String[] sa = bf.readLine().split(" ");
 
-		graph = new List[N + 1];
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < M; i++) {
-			sa = bf.readLine().split(" ");
-			int start = Integer.parseInt(sa[0]);
-			int end = Integer.parseInt(sa[1]);
+        // 정답
+        int human = Integer.MAX_VALUE;
+        int cnt = Integer.MAX_VALUE;
 
-			graph[start].add(end);
-			graph[end].add(start);
-		}
+        N = Integer.parseInt(sa[0]);
+        M = Integer.parseInt(sa[1]);
 
-		for (int i = 1; i <= N; i++) {
-			bfs(i);
-		}
+        list = new List[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-		System.out.println(humanIdx);
-	}
+        for (int i = 0; i < M; i++) {
+            sa = bf.readLine().split(" ");
+            int A = Integer.parseInt(sa[0]);
+            int B = Integer.parseInt(sa[1]);
 
-	public static void bfs(int y) {
-		int sum = 0;
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {y, 0});
-		boolean[] visited = new boolean[N + 1];
-		visited[y] = true;
+            list[A].add(B);
+            list[B].add(A);
+        }
 
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			int cur = temp[0];
-			int step = temp[1];
+        for (int i = 1; i < N + 1; i++) {
+            // 해당 사람의 케빈 베이컨 수
+            int[] result = new int[N + 1];
+            Arrays.fill(result, Integer.MAX_VALUE);
+            // 해당 사람의 방문 체크
+            boolean[] visited = new boolean[N + 1];
 
-			for (int i = 0; i < graph[cur].size(); i++) {
-				int next = graph[cur].get(i);
-				if (visited[next])
-					continue;
+            bfs(i, result, visited);
 
-				visited[next] = true;
-				sum += step + 1;
-				q.add(new int[] {next, step + 1});
-			}
-		}
+            // 해당 사람의 케빈 베이컨 수 합 계산
+            int tempResult = 0;
+            for (int j = 0; j < N + 1; j++) {
+                if (result[j] == Integer.MAX_VALUE) continue;
+                tempResult += result[j];
+            }
 
-		if (result == sum) {
-			humanIdx = Math.min(humanIdx, y);
-		} else if (result > sum) {
-			result = sum;
-			humanIdx = y;
-		}
-	}
+            // 정답 갱신
+            if (cnt > tempResult) {
+                human = i;
+                cnt = tempResult;
+            }
+        }
+
+        System.out.println(human);
+    }
+
+    public static void bfs(int i, int[] result, boolean[] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, 0});
+
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            int num = temp[0];
+            int cnt = temp[1];
+
+            if (visited[num]) continue;
+            visited[num] = true;
+            if (i != num) {
+                result[num] = Math.min(result[num], cnt);
+            }
+
+            for (int j = 0; j < list[num].size(); j++) {
+                int next = list[num].get(j);
+                q.add(new int[]{next, cnt + 1});
+            }
+        }
+    }
 
 }
