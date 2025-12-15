8eb90580d05efa122ddff4bc3ec9ef98a746b8f3
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-12-15 22:51:49 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 500 ms, Memory: 121028 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,84 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
 
-    static int[][] map;
-    static int N;
-    static int M;
-    // 상 우 하 좌
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
+    static int[][] arr;
     static boolean[][][] visited;
+    static int N, M;
+
+    // 상하좌우
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String[] sa = bf.readLine().split(" ");
         N = Integer.parseInt(sa[0]);
         M = Integer.parseInt(sa[1]);
 
-        map = new int[N][M];
-        visited = new boolean[N][M][2];
+        arr = new int[N][M];
+        visited = new boolean[2][N][M];
 
         for (int i = 0; i < N; i++) {
-            char[] s = bf.readLine().toCharArray();
+            sa = bf.readLine().split(" ");
             for (int j = 0; j < M; j++) {
-                map[i][j] = s[j] - 48;
+                arr[i][j] = sa[0].charAt(j) - '0';
             }
         }
 
-        int bfs = 0;
-        if (map[0][0] == 1) {
-            bfs = bfs(0, 0, 1, 1);
-        } else {
-            bfs = bfs(0, 0, 0, 1);
-        }
+        int reuslt = bfs();
 
-        if(N == 1 && M == 1){
+        if (N == 1 && M == 1) {
             System.out.println(1);
-        }else{
-            System.out.println(bfs);
+        } else {
+            System.out.println(reuslt);
         }
-
     }
 
-    public static int bfs(int y, int x, int wall, int step) {
+    public static int bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{y, x, wall, step});
-        visited[y][x][0] = true;
+        q.add(new int[]{0, 0, 1, 0});
+        visited[0][0][0] = true;
 
         while (!q.isEmpty()) {
             int[] temp = q.poll();
-            y = temp[0];
-            x = temp[1];
-            int cur_wall = temp[2];
-            int cur_step = temp[3];
+            int y = temp[0];
+            int x = temp[1];
+            int step = temp[2];
+            int wall = temp[3];
 
             for (int i = 0; i < 4; i++) {
-                int ndx = x + dx[i];
-                int ndy = y + dy[i];
-                if (ndx >= 0 && ndy >= 0 && ndx < M && ndy < N) {
-                    // 현재 위치가 벽인데 아직 부수지 않은 상태면
-                    if (map[ndy][ndx] == 1 && cur_wall == 0 && !visited[ndy][ndx][1]) {
-                        visited[ndy][ndx][1] = true;
-                        if (ndy == N - 1 && ndx == M - 1) {
-                            return cur_step + 1;
-                        }
-                        q.add(new int[]{ndy, ndx, 1, cur_step + 1});
-                    } else if (map[ndy][ndx] == 0 && !visited[ndy][ndx][cur_wall]) {
-                        visited[ndy][ndx][cur_wall] = true;
-                        if (ndy == N - 1 && ndx == M - 1) {
-                            return cur_step + 1;
-                        }
-                        q.add(new int[]{ndy, ndx, cur_wall, cur_step + 1});
-                    }
+                int ndy = dy[i] + y;
+                int ndx = dx[i] + x;
+                if (ndx < 0 || ndy < 0 || ndx >= M || ndy >= N) continue;
+
+                if (ndy == N - 1 && ndx == M - 1) {
+                    return step + 1;
                 }
+                if (arr[ndy][ndx] == 1) {
+                    // 부술 수 있는데 이미 방문했다면
+                    if (wall == 0) {
+                        if (visited[wall + 1][ndy][ndx]) {
+                            continue;
+                        }
+                        visited[wall + 1][ndy][ndx] = true;
+                        q.add(new int[]{ndy, ndx, step + 1, wall + 1});
+                    }
+                } else {
+                    if (visited[wall][ndy][ndx]) {
+                        continue;
+                    }
+                    visited[wall][ndy][ndx] = true;
+                    q.add(new int[]{ndy, ndx, step + 1, wall});
+                }
+
             }
         }
+
         return -1;
     }
 }
