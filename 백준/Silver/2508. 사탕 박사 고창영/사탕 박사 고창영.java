45d0ef14e1c78a790eb301fe7bd446988677d617
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2025-12-22 23:06:33 +0900

    [Silver V] Title: 사탕 박사 고창영, Time: 152 ms, Memory: 20840 KB -BaekjoonHub

--- 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.java
+++ 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.java
@@ -8,44 +8,43 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(bf.readLine());
+
         StringBuilder sb = new StringBuilder();
 
         for (int t = 0; t < T; t++) {
             bf.readLine();
+
             String[] sa = bf.readLine().split(" ");
-            int r = Integer.parseInt(sa[0]);
-            int c = Integer.parseInt(sa[1]);
+            
+            int N = Integer.parseInt(sa[0]);
+            int M = Integer.parseInt(sa[1]);
 
-            char[][] arr = new char[r][c];
+            char[][] arr = new char[N][M];
 
-            for (int i = 0; i < r; i++) {
+            for (int i = 0; i < N; i++) {
                 arr[i] = bf.readLine().toCharArray();
             }
 
             int candy = 0;
-            // 가로 사탕 계산
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c - 2; j++) {
-                    if (arr[i][j] == '>' && arr[i][j + 1] == 'o' && arr[i][j + 2] == '<') {
-                        candy++;
-                    }
+
+            // 가로
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M - 2; j++) {
+                    if (arr[i][j] == '>' && arr[i][j + 1] == 'o' && arr[i][j + 2] == '<') candy++;
                 }
             }
 
-            // 가로 사탕 계산
-            for (int j = 0; j < c; j++) {
-                for (int i = 0; i < r - 2; i++) {
-                    if (arr[i][j] == 'v' && arr[i + 1][j] == 'o' && arr[i + 2][j] == '^') {
-                        candy++;
-                    }
+            // 세로
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N - 2; j++) {
+                    if (arr[j][i] == 'v' && arr[j + 1][i] == 'o' && arr[j + 2][i] == '^') candy++;
                 }
             }
+
 
             sb.append(candy).append("\n");
         }
+
         System.out.println(sb);
-
-
     }
-
 }
