be532cd9a93583d6d2ffc82f5fa8a1666984ad98
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2026-03-06 22:55:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1220 ms, Memory: 196368 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,44 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    static int N, M;
-    static int[] takeCard;
-    static int[] searchCard;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String s = bf.readLine();
+
+        int N = Integer.parseInt(bf.readLine());
+        int[] sang = new int[N];
         String[] sa = bf.readLine().split(" ");
-        N = Integer.parseInt(s);
-        takeCard = new int[N];
-        for(int i=0; i<N; i++){
-            takeCard[i] = Integer.parseInt(sa[i]);
+        for (int i = 0; i < N; i++) {
+            sang[i] = Integer.parseInt(sa[i]);
         }
+        Arrays.sort(sang);
 
-        s = bf.readLine();
+        int M = Integer.parseInt(bf.readLine());
         sa = bf.readLine().split(" ");
-        M = Integer.parseInt(s);
-        searchCard = new int[M];
-        for(int i=0; i<M; i++){
-            searchCard[i] = Integer.parseInt(sa[i]);
-        }
-
-        int[] countArr = new int[20000002];
-        for(int i=0; i<N; i++){
-            countArr[10000000 + takeCard[i]]++;
-        }
-        int[] result = new int[M];
-        for(int i=0; i<M; i++){
-            result[i] = countArr[10000000 + searchCard[i]];
-        }
 
         StringBuilder sb = new StringBuilder();
-        for(int i : result){
-            sb.append(i).append(" ");
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(sa[i]);
+            int count = upperBound(sang, target) - lowerBound(sang, target);
+            sb.append(count).append(" ");
         }
+
         System.out.println(sb);
+
+    }
+
+    static int lowerBound(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (arr[m] >= target) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return s;
+    }
+
+    static int upperBound(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int m = (s + e) / 2;
+            if (arr[m] > target) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return s;
     }
 }
