af35ac1db2d2b84b1e1cb2327db13008cea0d6a4
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2026-03-27 23:21:48 +0900

    [Silver IV] Title: 수들의 합 2, Time: 172 ms, Memory: 14768 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -4,43 +4,36 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] sa = bf.readLine().split(" ");
-		int N = Integer.parseInt(sa[0]);
-		int M = Integer.parseInt(sa[1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] arr = new int[N];
-		sa = bf.readLine().split(" ");
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(sa[i]);
-		}
+        String[] sa = bf.readLine().split(" ");
+        int N = Integer.parseInt(sa[0]);
+        int M = Integer.parseInt(sa[1]);
 
-		int cnt = 0;
-		int s = 0;
-		int e = 0;
-		int sum = 0;
+        int[] arr = new int[N];
+        sa = bf.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(sa[i]);
+        }
 
-		while (e <= N) {
-			if (sum < M) {
-				if (e < N) {
-					sum += arr[e];
-					e++;
-				} else {
-					break;
-				}
-			} else if (sum > M) {
-				sum -= arr[s];
-				s++;
-			} else {
-				cnt++;
-				sum -= arr[s];
-				s++;
-			}
-		}
+        int[] prefixSum = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
+        }
 
-		System.out.println(cnt);
-	}
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                if (prefixSum[j] - prefixSum[i] == M) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 
 }
