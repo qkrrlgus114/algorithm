7e4b6a5bcb858fe4c249f65ca466d8eaecb15ba2
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2026-04-08 20:31:08 +0900

    [Silver II] Title: N과 M (9), Time: 208 ms, Memory: 30260 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -2,25 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-    static int N, M;
-    static boolean[] visited = new boolean[10001];
-    static StringBuilder sb = new StringBuilder();
-    static int[] answer;
     static int[] arr;
+    static int[] temp;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    static StringBuilder tempSb = new StringBuilder();
+    static boolean[] visited;
+    static Set<String> set = new HashSet<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sa = bf.readLine().split(" ");
-
         N = Integer.parseInt(sa[0]);
         M = Integer.parseInt(sa[1]);
 
-        answer = new int[M];
         arr = new int[N];
+        temp = new int[M];
         visited = new boolean[N];
 
         sa = bf.readLine().split(" ");
@@ -29,28 +34,36 @@ public class Main {
         }
         Arrays.sort(arr);
 
-        recur(0);
+        dfs(0);
 
         System.out.println(sb);
     }
 
-    public static void recur(int depth) {
+    public static void dfs(int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                sb.append(answer[i]).append(" ");
+                tempSb.append(temp[i]).append(" ");
             }
+            if (set.contains(tempSb.toString())) {
+                tempSb.setLength(0);
+                return;
+            }
+            for (int i = 0; i < M; i++) {
+                sb.append(temp[i]).append(" ");
+            }
+            set.add(tempSb.toString());
+            tempSb.setLength(0);
             sb.append("\n");
             return;
         }
 
-        int before = -1;
         for (int i = 0; i < N; i++) {
-            if (visited[i] || before == arr[i]) continue;
-            before = arr[i];
+            if (visited[i]) continue;
+            temp[depth] = arr[i];
             visited[i] = true;
-            answer[depth] = arr[i];
-            recur(depth + 1);
+            dfs(depth + 1);
             visited[i] = false;
         }
     }
+
 }
