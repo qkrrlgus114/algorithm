64d8a5b064cd48c6846b3df7dff02212555a1f9f
박기현 <121294224+qkrrlgus114@users.noreply.github.com>
2026-04-10 23:01:43 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15460 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,39 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    static int X;
-    static int[] memo;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        X = Integer.parseInt(bf.readLine());
-        memo = new int[X + 1];
+        int N = Integer.parseInt(bf.readLine());
+        dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
 
-        int answer = recur(X);
-
-        System.out.println(answer);
+        System.out.println(combination(N));
     }
 
-    public static int recur(int num) {
-        if (num == 1) {
-            return 0;
+    public static int combination(int value) {
+        dp[1] = 0;
+
+        for (int i = 2; i <= value; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
         }
 
-        if (memo[num] != 0) return memo[num];
-
-        int min = Integer.MAX_VALUE;
-
-        if (num % 3 == 0) min = Math.min(min, recur(num / 3) + 1);
-        if (num % 2 == 0) min = Math.min(min, recur(num / 2) + 1);
-        min = Math.min(min, recur(num - 1) + 1);
-
-        return memo[num] = min;
+        return dp[value];
     }
-
-
 }
